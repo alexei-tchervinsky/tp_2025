4eb75758a997b6d3be034b1142b131a83e234be8
rabbitdfs <miroslav_ponomarenko@bk.ru>
2025-04-04 18:13:09 +0300

    Initial commit for T0

---
+++ ponomarenko.miroslav/T0/T0/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  T0
+//
+//  Created by Мирослав Пономаренко on 04.04.2025.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    
+    return 0;
+}
