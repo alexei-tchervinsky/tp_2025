810e8345824918da773c376606ac1f94586f7b26
foriinrage <eliseypylenka@gmail.com>
2025-05-01 18:06:15 +0300

    errors proccessing

--- pylenkov.elisey/T2/main.cpp
+++ pylenkov.elisey/T2/main.cpp
@@ -21,21 +21,56 @@ struct DataStruct
         {
             ds.key1 = line[key1_start + 7];
         }
+        else
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
 
         size_t key2_start = line.find(":key2 0x");
         if (key2_start != std::string::npos)
         {
             size_t key2_end = line.find(":", key2_start + 8);
+            if (key2_end == std::string::npos)
+            {
+                is.setstate(std::ios::failbit);
+                return is;
+            }
             std::string hex_str = line.substr(key2_start + 8, key2_end - key2_start - 8);
+            if (hex_str.size() == 0)
+            {
+                is.setstate(std::ios::failbit);
+                return is;
+            }
             ds.key2 = std::stoull(hex_str, nullptr, 16);
 
+        }
+        else
+        {
+            is.setstate(std::ios::failbit);
+            return is;
         }
 
         size_t key3_start = line.find(":key3 \"");
         if (key3_start != std::string::npos)
         {
             size_t key3_end = line.find('"', key3_start + 7);
+            if (key3_end == std::string::npos)
+            {
+                is.setstate(std::ios::failbit);
+                return is;
+            }
             ds.key3 = line.substr(key3_start + 7, key3_end - key3_start - 7);
+            if (ds.key3.size() == 0)
+            {
+                is.setstate(std::ios::failbit);
+                return is;
+            }
+        }
+        else
+        {
+            is.setstate(std::ios::failbit);
+            return is;
         }
 
         return is; // чтобы поддерживать цепочку операций ввода
@@ -60,9 +95,17 @@ bool compare(const DataStruct& a, const DataStruct& b)
 int main()
 {
     std::vector<DataStruct> data;
-
-    std::istream_iterator<DataStruct> start(std::cin), end;
-    std::copy(start, end, std::back_inserter(data));
+    while(!std::cin.eof())
+    {
+        std::copy(std::istream_iterator<DataStruct> (std::cin),
+        std::istream_iterator<DataStruct> (),
+        std::back_inserter(data));
+        if(!std::cin)
+        {
+            std::cin.clear();
+            std::cin.ignore(100, '\n');
+        }
+    }
 
     std::sort(data.begin(), data.end(), compare);
 
