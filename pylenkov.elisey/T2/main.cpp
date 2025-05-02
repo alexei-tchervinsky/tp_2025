0d8871633671622b3087b732078400deef96a7c3
foriinrage <eliseypylenka@gmail.com>
2025-05-02 14:00:40 +0300

    errors proccessing #2

--- pylenkov.elisey/T2/main.cpp
+++ pylenkov.elisey/T2/main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <string>
 #include <iterator>
+#include <sstream>
 
 struct DataStruct
 {
@@ -10,80 +11,75 @@ struct DataStruct
     unsigned long long key2;
     std::string key3;
 
-    // перегруженный оператор ввода
     friend std::istream& operator>>(std::istream& is, DataStruct& ds)
     {
         std::string line;
         if (!std::getline(is, line)) return is;
 
+        // проверка key1
         size_t key1_start = line.find(":key1 '");
-        if (key1_start != std::string::npos)
+        if (key1_start == std::string::npos || key1_start + 8 > line.size())
         {
-            ds.key1 = line[key1_start + 7];
+            is.setstate(std::ios::failbit);
+            return is;
         }
-        else
+        ds.key1 = line[key1_start + 7];
+        if (line[key1_start + 8] != '\'')
         {
             is.setstate(std::ios::failbit);
             return is;
         }
 
+        // проверка key2
         size_t key2_start = line.find(":key2 0x");
-        if (key2_start != std::string::npos)
+        if (key2_start == std::string::npos)
         {
-            size_t key2_end = line.find(":", key2_start + 8);
-            if (key2_end == std::string::npos)
-            {
-                is.setstate(std::ios::failbit);
-                return is;
-            }
-            std::string hex_str = line.substr(key2_start + 8, key2_end - key2_start - 8);
-            if (hex_str.size() == 0)
-            {
-                is.setstate(std::ios::failbit);
-                return is;
-            }
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+        size_t key2_end = line.find(':', key2_start + 8);
+        if (key2_end == std::string::npos)
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+        std::string hex_str = line.substr(key2_start + 8, key2_end - (key2_start + 8));
+        try
+        {
             ds.key2 = std::stoull(hex_str, nullptr, 16);
-
         }
-        else
+        catch (...)
         {
             is.setstate(std::ios::failbit);
             return is;
         }
 
+        // проверка key3
         size_t key3_start = line.find(":key3 \"");
-        if (key3_start != std::string::npos)
-        {
-            size_t key3_end = line.find('"', key3_start + 7);
-            if (key3_end == std::string::npos)
-            {
-                is.setstate(std::ios::failbit);
-                return is;
-            }
-            ds.key3 = line.substr(key3_start + 7, key3_end - key3_start - 7);
-            if (ds.key3.size() == 0)
-            {
-                is.setstate(std::ios::failbit);
-                return is;
-            }
-        }
-        else
+        if (key3_start == std::string::npos)
         {
             is.setstate(std::ios::failbit);
             return is;
         }
+        size_t key3_end = line.find('"', key3_start + 7);
+        if (key3_end == std::string::npos)
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+        ds.key3 = line.substr(key3_start + 7, key3_end - (key3_start + 7));
 
-        return is; // чтобы поддерживать цепочку операций ввода
+        return is;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const DataStruct& ds)
     {
-        os << "(:key1 '" << ds.key1 << "':key2 0x" << std::hex << std::uppercase << ds.key2 << std::nouppercase << ":key3 \"" << ds.key3 << "\":)";
-        return os; // чтобы поддерживать цепочку операций вывода
+        return os << "(:key1 '" << ds.key1 << "':key2 0x"
+                 << std::hex << std::uppercase << ds.key2 << std::nouppercase << std::dec
+                 << ":key3 \"" << ds.key3 << "\":)";
     }
 };
 
-// собственный компаратор
 bool compare(const DataStruct& a, const DataStruct& b)
 {
     if (a.key1 != b.key1) return a.key1 < b.key1;
@@ -91,25 +87,27 @@ bool compare(const DataStruct& a, const DataStruct& b)
     return a.key3.length() < b.key3.length();
 }
 
-
 int main()
 {
     std::vector<DataStruct> data;
-    while(!std::cin.eof())
+    std::string line;
+
+    while (std::getline(std::cin, line))
     {
-        std::copy(std::istream_iterator<DataStruct> (std::cin),
-        std::istream_iterator<DataStruct> (),
-        std::back_inserter(data));
-        if(!std::cin)
+        std::istringstream iss(line);
+        DataStruct ds;
+        if (iss >> ds)
         {
-            std::cin.clear();
-            std::cin.ignore(100, '\n');
+            data.push_back(ds);
         }
     }
 
     std::sort(data.begin(), data.end(), compare);
 
-    std::ostream_iterator<DataStruct> out(std::cout, "\n");
-    std::copy(data.begin(), data.end(), out);
+    for (const auto& item : data)
+    {
+        std::cout << item << '\n';
+    }
+
     return 0;
 }
