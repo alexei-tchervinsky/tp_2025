98de01c98e0578d936e5b389aa043fd9e5ca951f
Marfina <almarfina@gmail.com>
2025-05-03 16:06:26 +0300

    T2

--- marfina.alisa/T2/main.cpp
+++ marfina.alisa/T2/main.cpp
@@ -8,15 +8,12 @@
 int main()
 {
     std::vector<marfina::DataStruct> data;
-    bool has_supported = false;
-
     while (!std::cin.eof())
     {
         marfina::DataStruct tmp;
         if (std::cin >> tmp)
         {
             data.push_back(tmp);
-            has_supported = true;
         }
         else if (std::cin.fail())
         {
@@ -24,19 +21,10 @@ int main()
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
-
-    if (!has_supported)
-    {
-        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
-        return 0;
-    }
-
     std::sort(data.begin(), data.end(), marfina::compare_structures);
-    std::cout << "Atleast one supported record type\n";
     for (const auto& item : data)
     {
         std::cout << item << "\n";
     }
-
     return 0;
 }
