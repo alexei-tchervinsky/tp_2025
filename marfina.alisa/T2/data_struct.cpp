7458a70e2c474f1fde863980967e9f8041623d1c
Marfina <almarfina@gmail.com>
2025-05-03 13:58:07 +0300

    T2 commit

---
+++ marfina.alisa/T2/data_struct.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "data_struct.hpp"
+
+namespace marfina
+{
+
+std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+    char c;
+    if (in >> c && c != dest.exp) in.setstate(std::ios::failbit);
+    return in;
+}
+
+std::istream& operator>>(std::istream& in, CharIO&& dest)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+    char quote;
+    if (in >> quote && quote != '\'')
+    {
+        in.setstate(std::ios::failbit);
+        return in;
+    }
+    in >> dest.ref >> quote;
+    if (quote != '\'') in.setstate(std::ios::failbit);
+    return in;
+}
+
+std::istream& operator>>(std::istream& in, RationalIO&& dest)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+    in >> DelimiterIO{'('} >> DelimiterIO{':'} >> LabelIO{"N"};
+    in >> dest.ref.first;
+    in >> DelimiterIO{':'} >> LabelIO{"D"};
+    in >> dest.ref.second;
+    in >> DelimiterIO{':'} >> DelimiterIO{')'};
+    if (dest.ref.second == 0) in.setstate(std::ios::failbit);
+    return in;
+}
+
+std::istream& operator>>(std::istream& in, StringIO&& dest)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+    return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
+}
+
+std::istream& operator>>(std::istream& in, LabelIO&& dest)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+    std::string data;
+    if ((in >> data) && (data != dest.exp))
+    {
+        in.setstate(std::ios::failbit);
+    }
+    return in;
+}
+std::istream& operator>>(std::istream& in, DataStruct& dest)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+
+    DataStruct input;
+    bool has_key1 = false, has_key2 = false, has_key3 = false;
+
+    in >> DelimiterIO{'('} >> DelimiterIO{':'};
+
+    while (true)
+    {
+        if (in.peek() == ')')
+        {
+            in.ignore();
+            break;
+        }
+
+        std::string field;
+        if (!(in >> field)) break;
+
+        if (field == "key1")
+        {
+            if (in.peek() == '"')
+            {
+                std::string str_val;
+                if (in >> StringIO{str_val} && !str_val.empty())
+                {
+                    input.key1.first = static_cast<long long>(str_val[0]);
+                    input.key1.second = 1;
+                    has_key1 = true;
+                }
+            }
+            else if (in.peek() == '(')
+            {
+                if (in >> RationalIO{input.key1})
+                {
+                    has_key1 = true;
+                }
+            }
+            in >> DelimiterIO{':'};
+        }
+        else if (field == "key2")
+        {
+            if (in.peek() == '\'')
+            {
+                in >> CharIO{input.key2};
+                has_key2 = true;
+            }
+            else if (in.peek() == '(')
+            {
+                std::pair<long long, unsigned long long> tmp;
+                if (in >> RationalIO{tmp})
+                {
+                    input.key2 = static_cast<char>(tmp.first);
+                    has_key2 = true;
+                }
+            }
+            in >> DelimiterIO{':'};
+        }
+        else if (field == "key3")
+        {
+            in >> StringIO{input.key3} >> DelimiterIO{':'};
+            has_key3 = true;
+        }
+    }
+
+    if (has_key1 && has_key2 && has_key3)
+    {
+        dest = input;
+    }
+    else
+    {
+        in.setstate(std::ios::failbit);
+    }
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const DataStruct& src)
+{
+    std::ostream::sentry sentry(out);
+    if (!sentry) return out;
+    iofmtguard fmtguard(out);
+    out << "(:key1 ";
+    if (src.key1.second == 1 && isprint(static_cast<char>(src.key1.first))) {
+        out << "\"" << static_cast<char>(src.key1.first) << "\"";
+    } else {
+        out << "(:N " << src.key1.first << ":D " << src.key1.second << ":)";
+    }
+    out << ":key2 '" << src.key2 << "'"
+        << ":key3 \"" << src.key3 << "\":)";
+    return out;
+}
+
+iofmtguard::iofmtguard(std::basic_ios<char>& s) :
+    s_(s), width_(s.width()), fill_(s.fill()),
+    precision_(s.precision()), fmt_(s.flags()){}
+
+iofmtguard::~iofmtguard()
+{
+    s_.width(width_);
+    s_.fill(fill_);
+    s_.precision(precision_);
+    s_.flags(fmt_);
+}
+
+bool compare_structures(const DataStruct& a, const DataStruct& b)
+{
+    if (a.key1 != b.key1) return a.key1 < b.key1;
+    if (a.key2 != b.key2) return a.key2 < b.key2;
+    return a.key3.length() < b.key3.length();
+}
+}
---
+++ marfina.alisa/T2/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <limits>
+#include "data_struct.hpp"
+
+int main()
+{
+    std::vector<marfina::DataStruct> data;
+    bool has_supported = false;
+
+    while (!std::cin.eof())
+    {
+        marfina::DataStruct tmp;
+        if (std::cin >> tmp)
+        {
+            data.push_back(tmp);
+            has_supported = true;
+        }
+        else if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+
+    if (!has_supported)
+    {
+        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
+        return 0;
+    }
+
+    std::sort(data.begin(), data.end(), marfina::compare_structures);
+    std::cout << "Atleast one supported record type\n";
+    for (const auto& item : data)
+    {
+        std::cout << item << "\n";
+    }
+
+    return 0;
+}
