ac3fefce0cfd6aef4e895e5145ff7dc675ad2a63
Marfina <almarfina@gmail.com>
2025-05-03 16:28:24 +0300

    T2_2

--- marfina.alisa/T2/main.cpp
+++ marfina.alisa/T2/main.cpp
@@ -8,24 +8,38 @@
 int main()
 {
     std::vector<marfina::DataStruct> data;
+    bool has_supported = false;
+
+    // Чтение данных с обработкой ошибок
     while (!std::cin.eof())
     {
-        std::copy
-        (
-            std::istream_iterator<DataStruct>(std::cin),
-            std::istream_iterator<DataStruct>(),
-            std::back_inserter(data)
-        );
-        if (std::cin.fail())
+        marfina::DataStruct tmp;
+        if (std::cin >> tmp)
+        {
+            data.push_back(tmp);
+            has_supported = true;
+        }
+        else if (std::cin.fail())
         {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
+
+    // Проверка наличия корректных записей
+    if (!has_supported)
+    {
+        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
+        return 0;
+    }
+
+    // Сортировка и вывод
     std::sort(data.begin(), data.end(), marfina::compare_structures);
-    std::copy
-    (
-        data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n")
-    );
+    std::cout << "Atleast one supported record type\n";
+    for (const auto& item : data)
+    {
+        std::cout << item << "\n";
+    }
+
     return 0;
 }
