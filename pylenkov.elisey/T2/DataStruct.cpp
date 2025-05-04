39764ba897deb4b17bfd3ab4b3112fd3f1e7fac9
foriinrage <eliseypylenka@gmail.com>
2025-05-04 18:28:59 +0300

    OOP fix2

---
+++ pylenkov.elisey/T2/DataStruct.cpp
@@ -0,0 +1,74 @@
+#include "DataStruct.hpp"
+#include "iofmtguard.hpp"
+#include "IO_Objects.hpp"
+
+namespace nspace
+{
+    std::istream& operator>>(std::istream& is, DataStruct& ds)
+    {
+        std::istream::sentry sentry(is);
+        if(!sentry)
+        {
+            return is;
+        }
+        DataStruct temp;
+        is >> DelimiterIO { '(' };
+        for(std::size_t i = 0; i < 3; ++i)
+        {
+            short number;
+            is >> DelimiterIO { ':' } >> LabelIO { "key" } >> number;
+            switch(number)
+            {
+                case 1:
+                {
+                    is >> CharIO { temp.key1 };
+                    break;
+                }
+                case 2:
+                {
+                    is >> ULLHexIO { temp.key2 };
+                    break;
+                }
+                case 3:
+                {
+                    if (!(is >> StringIO { temp.key3 }))
+                    {
+                        is.setstate(std::ios::failbit);
+                        return is;
+                    }
+                    break;
+                }
+                default:
+                    is.setstate(std::ios::failbit);
+                    return is;
+            }
+        }
+        is >> DelimiterIO { ':' } >> DelimiterIO { ')' };
+        if(is)
+        {
+            ds = temp;
+        }
+        return is;
+    }
+
+
+    std::ostream& operator<<(std::ostream &os, const DataStruct &ds)
+    {
+        std::ostream::sentry sentry(os);
+        if (!sentry)
+        {
+            return os;
+        }
+        iofmtguard fmtguard(os);
+        os << "(:key1 '" << ds.key1 << "':key2 0x" << ds.key2 << ":key3 \"" << ds.key3 << "\":)";
+
+        return os;
+    }
+
+    bool compare(const DataStruct &ds1, const DataStruct &ds2)
+    {
+        if (ds1.key1 != ds2.key1) return ds1.key1 < ds2.key1;
+        if (ds1.key2 != ds2.key2) return ds1.key2 < ds2.key2;
+        return ds1.key3.size() < ds2.key3.size();
+    }
+}
---
+++ pylenkov.elisey/T2/IO_Objects.cpp
@@ -0,0 +1,118 @@
+#include "IO_Objects.hpp"
+#include <iterator>
+#include <algorithm>
+
+namespace nspace
+{
+    std::istream& operator>>(std::istream& is, DelimiterIO&& dlim)
+    {
+        std::istream::sentry sentry(is);
+        if(!sentry)
+        {
+            return is;
+        }
+
+        char c = '0';
+        is >> c;
+
+        if (is && (c != dlim.exp))
+        {
+            is.setstate(std::ios::failbit);
+        }
+        return is;
+    }
+
+    std::istream& operator>>(std::istream& is, LabelIO&& l)
+    {
+        std::istream::sentry sentry(is);
+        if(!sentry)
+        {
+            return is;
+        }
+        std::string temp;
+        std::copy_n(
+            std::istream_iterator<char>(is),
+            l.label.length(),
+            std::back_inserter(temp)
+        );
+        if(is && (temp != l.label))
+        {
+            is.setstate(std::ios::failbit);
+        }
+        return is;
+    }
+
+    std::istream& operator>>(std::istream& is, CharIO&& chr)
+    {
+        std::istream::sentry sentry(is);
+        if (!sentry)
+        {
+            return is;
+        }
+
+        char quote;
+        is >> quote;
+        if (quote != '\'')
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+
+        char c;
+        is >> c;
+        chr.ref = c;
+
+        is >> quote;
+        if (quote != '\'')
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+
+        return is;
+    }
+
+    std::istream& operator>>(std::istream& is, ULLHexIO&& ullhex)
+    {
+        std::istream::sentry sentry(is);
+        if (!sentry)
+        {
+            return is;
+        }
+
+        char c1, c2;
+        is >> c1 >> c2;
+        if(c1 != '0' || (c2 != 'x' && c2 != 'X'))
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+
+        std::string hex_num;
+        while(isalnum(is.peek())) {
+            char c;
+            is >> c;
+            hex_num += c;
+        }
+
+        try
+        {
+            ullhex.ref = std::stoull(hex_num, nullptr, 16);
+        }
+        catch(...)
+        {
+            is.setstate(std::ios::failbit);
+        }
+        return is;
+    }
+
+    std::istream& operator>>(std::istream& is, StringIO&& s)
+    {
+        std::istream::sentry sentry(is);
+        if(!sentry)
+        {
+            return is;
+        }
+        return std::getline(is >> DelimiterIO { '\"' }, s.ref, '\"');
+    }
+}
