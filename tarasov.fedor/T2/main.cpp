b1ac62234912ced7231fec02451dc481b444fa0f
WellC2G <fedorka2010@gmail.com>
2025-05-05 13:31:08 +0300

    main.cpp

--- tarasov.fedor/T2/main.cpp
+++ tarasov.fedor/T2/main.cpp
@@ -11,25 +11,23 @@ using namespace tarasov;
 
 int main() {
     std::vector<DataStruct> data;
-
-    std::copy(
-        std::istream_iterator<DataStruct>(std::cin),
-        std::istream_iterator<DataStruct>(),
-        std::back_inserter(data)
-    );
-
-    if (std::cin.fail() && !std::cin.eof()) {
+    while (!std::cin.eof()) {
+        std::copy(
+            std::istream_iterator<DataStruct>(std::cin),
+            std::istream_iterator<DataStruct>(),
+            std::back_inserter(data)
+        );
+        if (!std::cin.fail()) {
+            continue;
+        }
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-
     std::sort(data.begin(), data.end());
-
     std::copy(
         data.begin(),
         data.end(),
         std::ostream_iterator<DataStruct>(std::cout, "\n")
     );
-
     return 0;
 }
