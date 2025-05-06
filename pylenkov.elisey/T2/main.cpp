5eac906b74b73e9a73b294102e973d8d65048dcd
foriinrage <eliseypylenka@gmail.com>
2025-05-06 21:47:56 +0300

    T2 v2

--- pylenkov.elisey/T2/main.cpp
+++ pylenkov.elisey/T2/main.cpp
@@ -1,71 +1,43 @@
-#include <iostream>
+#include "DataStruct.hpp"
 #include <vector>
+#include <iostream>
+#include <iterator>
 #include <algorithm>
-#include <string>
+#include <limits>
 
-struct DataStruct
+using nspace::DataStruct;
+
+int main()
 {
-    char key1;
-    unsigned long long key2;
-    std::string key3;
+    std::vector<DataStruct> ds;
 
-    // перегруженный оператор ввода 
-    friend std::istream& operator>>(std::istream& is, DataStruct& ds)
+    while (true)
     {
-        std::string line;
-        if (!std::getline(is, line)) return is;
+        DataStruct temp;
 
-        size_t key1_start = line.find(":key1 '");
-        if (key1_start != std::string::npos)
+        if (!(std::cin >> temp))
         {
-            ds.key1 = line[key1_start + 7];
+            if (std::cin.eof())
+            {
+                break;
+            }
+
+
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
         }
 
-        size_t key2_start = line.find(":key2 0x");
-        if (key2_start != std::string::npos)
-        {
-            size_t key2_end = line.find(":", key2_start + 8);
-            std::string hex_str = line.substr(key2_start + 8, key2_end - key2_start - 8);
-            ds.key2 = std::stoull(hex_str, nullptr, 16);
-
-        }
-
-        size_t key3_start = line.find(":key3 \"");
-        if (key3_start != std::string::npos)
-        {
-            size_t key3_end = line.find('"', key3_start + 7);
-            ds.key3 = line.substr(key3_start + 7, key3_end - key3_start - 7);
-        }
-
-        return is; // чтобы поддерживать цепочку операций ввода
+        ds.push_back(temp);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const DataStruct& ds)
-    {
-        os << "(:key1 '" << ds.key1 << "':key2 0x" << std::hex << std::uppercase << ds.key2 << std::nouppercase << ":key3 \"" << ds.key3 << "\":)";
-        return os; // чтобы поддерживать цепочку операций вывода
-    }
-};
+    std::sort(ds.begin(), ds.end(), nspace::compare);
 
-// собственный компаратор
-bool compare(const DataStruct& a, const DataStruct& b) 
-{
-    if (a.key1 != b.key1) return a.key1 < b.key1;
-    if (a.key2 != b.key2) return a.key2 < b.key2;
-    return a.key3.length() < b.key3.length();
-}
+    std::copy(
+        ds.begin(),
+        ds.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
 
-
-int main() 
-{
-    std::vector<DataStruct> data;
-
-    std::istream_iterator<DataStruct> start(std::cin), end;
-    std::copy(start, end, std::back_inserter(data));
-
-    std::sort(data.begin(), data.end(), compare);
-
-    std::ostream_iterator<DataStruct> out(std::cout, "\n");
-    std::copy(data.begin(), data.end(), out);
     return 0;
 }
