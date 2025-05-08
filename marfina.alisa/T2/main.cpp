8cced9b3c14b306d2cc2a9c7d6bcc46a2e1c9603
Marfina <almarfina@gmail.com>
2025-05-08 22:17:07 +0300

    added normal insert in main.cpp

--- marfina.alisa/T2/main.cpp
+++ marfina.alisa/T2/main.cpp
@@ -8,21 +8,21 @@
 int main() {
     std::vector<marfina::DataStruct> data;
 
-    while (!std::cin.eof()) {
-        marfina::DataStruct temp;
-        if (std::cin >> temp) {
-            data.push_back(temp);
-        }
-         else
+    while(!std::cin.eof())
+    {
+        std::copy(
+        std::istream_iterator<marfina::DataStruct >(std::cin),
+        std::istream_iterator<marfina::DataStruct >(),
+        std::back_inserter(data));
+        if (std::cin.fail())
         {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
-
     std::sort(data.begin(), data.end(), marfina::compare_structures);
-
-    std::copy(
+    std::copy
+    (
         data.begin(),
         data.end(),
         std::ostream_iterator<marfina::DataStruct>(std::cout, "\n")
