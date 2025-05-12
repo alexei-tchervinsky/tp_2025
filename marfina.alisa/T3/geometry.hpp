cb3dc35ef3c74159a015161d105c819a4c440437
Marfina <almarfina@gmail.com>
2025-05-12 19:28:22 +0300

    fixed geometry

--- marfina.alisa/T3/geometry.hpp
+++ marfina.alisa/T3/geometry.hpp
@@ -4,29 +4,43 @@
 #include <vector>
 #include <string>
 #include <functional>
+#include <iosfwd>
 
 struct Point
 {
     int x, y;
 };
-
 struct Polygon
 {
     std::vector<Point> points;
 };
+struct BoundingBox
+{
+    Point min;
+    Point max;
+};
 
-bool operator==(const Point& lhs, const Point& rhs);
-bool operator!=(const Point& lhs, const Point& rhs);
+struct AreaComparator
+{
+    bool operator()(const Polygon& a, const Polygon& b) const;
+};
+
+struct VertexCountComparator
+{
+    bool operator()(const Polygon& a, const Polygon& b) const;
+};
 double calculateArea(const Polygon& polygon);
 double calculateArea(const std::vector<Polygon>& polygons);
-std::vector<Polygon> readPolygonsFromFile(const std::string& filename);
-void processCommands(const std::vector<Polygon>& polygons);
-
+BoundingBox calculateBoundingBox(const std::vector<Polygon>& polygons);
+bool isPointInBoundingBox(const Point& p, const BoundingBox& box);
 void areaCommand(const std::vector<Polygon>& polygons, const std::string& arg);
-void maxCommand(const std::vector<Polygon>& polygons, const std::string& arg);
-void minCommand(const std::vector<Polygon>& polygons, const std::string& arg);
+void maxMinCommand(const std::vector<Polygon>& polygons, const std::string& type, const std::string& arg);
 void countCommand(const std::vector<Polygon>& polygons, const std::string& arg);
 void inframeCommand(const std::vector<Polygon>& polygons, const std::string& arg);
 void intersectionsCommand(const std::vector<Polygon>& polygons, const std::string& arg);
+void echoCommand(std::vector<Polygon>& polygons, const std::string& arg);
+void processCommands(std::vector<Polygon>& polygons);
+std::vector<Polygon> readPolygonsFromFile(const std::string& filename);
+bool parsePolygon(const std::string& str, Polygon& out);
 
 #endif
