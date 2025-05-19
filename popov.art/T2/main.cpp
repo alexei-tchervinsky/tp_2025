d8eacec4737b554b11d19c47192b288c3fa4782f
temaPop1e <popov.artoym7@gmail.com>
2025-05-20 02:58:02 +0300

    Update main.cpp2

--- popov.art/T2/main.cpp
+++ popov.art/T2/main.cpp
@@ -6,12 +6,24 @@
 int main()
 {
     std::ios_base::sync_with_stdio(false);
-    std::vector<DataStruct> v;
-    std::copy(std::istream_iterator<DataStruct>(std::cin),
-              std::istream_iterator<DataStruct>(),
-              std::back_inserter(v));
-    std::sort(v.begin(), v.end());
-    std::copy(v.begin(), v.end(),
-              std::ostream_iterator<DataStruct>(std::cout,"\n"));
+    std::vector<DataStruct> records;
+    std::size_t linesRead = 0;
+    for (std::string dummy; std::getline(std::cin, dummy); ++linesRead)
+    {
+        std::istringstream tmp(dummy);
+        DataStruct ds;
+        if (tmp >> ds) records.push_back(std::move(ds));
+    }
+    if (records.empty())
+    {
+        if (linesRead == 0)
+            std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
+        else
+            std::cout << "Atleast one supported record type\n";
+        return 0;
+    }
+    std::sort(records.begin(), records.end());
+    std::copy(records.begin(), records.end(),
+              std::ostream_iterator<DataStruct>(std::cout, "\n"));
     return 0;
 }
