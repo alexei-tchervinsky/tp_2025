4ea1187e311508c4d0b99179565d4a612f00979e
rabbitdfs <miroslav_ponomarenko@bk.ru>
2025-05-20 00:52:18 +0300

    remove trailing whitespace in main.cpp

--- ponomarenko.miroslav/T2/main.cpp
+++ ponomarenko.miroslav/T2/main.cpp
@@ -7,7 +7,6 @@
 int main() {
     std::vector<ponomarenko::DataStruct> data;
     ponomarenko::DataStruct temp;
-    
     while (!std::cin.eof()) {
         if (std::cin >> temp) {
             data.push_back(temp);
@@ -16,13 +15,10 @@ int main() {
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
-    
     std::sort(data.begin(), data.end());
-    
     for (const auto& el : data) {
         std::cout << el << "\n";
     }
-    
+
     return 0;
-    
 }
