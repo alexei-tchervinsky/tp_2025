fcee39e1315da7d0f52226de7100d1e402500731
Тимур Ахметханов <teimuran@Teimurxn.local>
2025-05-25 16:09:18 +0300

    first T2

---
+++ akhmetkhanov.timur/T2/T2/T2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  T2
+//
+//  Created by Тимур Ахметханов on 25.05.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
