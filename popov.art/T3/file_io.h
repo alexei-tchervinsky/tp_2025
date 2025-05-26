68f508b703c823f054d8cad6f9020b5f75cb6cd3
temaPop1e <popov.artoym7@gmail.com>
2025-05-26 21:04:44 +0300

    Update file_io.h3

--- popov.art/T3/file_io.h
+++ popov.art/T3/file_io.h
@@ -1,11 +1,22 @@
 #ifndef FILE_IO_H
 #define FILE_IO_H
-
 #include "geometry.h"
 #include <vector>
 #include <string>
-
+#include <sstream>
+#include <iostream>
 std::vector<Polygon> readPolygonsFromFile(const std::string& filename);
-Polygon parsePolygon(const std::string& input);
-
+inline Polygon parsePolygon(const std::string& input) {
+    std::istringstream iss(input);
+    int vertexCount;
+    iss >> vertexCount;
+    Polygon poly;
+    for (int i = 0; i < vertexCount; ++i) {
+        char c1, c2, c3;
+        int x, y;
+        iss >> c1 >> x >> c2 >> y >> c3;
+        poly.points.push_back({x, y});
+    }
+    return poly;
+}
 #endif // FILE_IO_H
