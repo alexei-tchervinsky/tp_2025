365b2757a6cc6b0216425dc6892550f7e52b7a46
temaPop1e <popov.artoym7@gmail.com>
2025-06-06 22:14:34 +0300

    Update commands.cpp17

--- popov.art/T3/commands.cpp
+++ popov.art/T3/commands.cpp
@@ -3,18 +3,21 @@
 #include <functional>
 #include <map>
 #include <vector>
-popov::CommandMap popov::createCommandMap(std::vector<Polygon>& polygons)
+namespace popov
 {
-  CommandMap commands;
-  using namespace std::placeholders;
-  commands["AREA"] = std::bind(area, std::cref(polygons), _1, _2);
-  commands["COUNT"] = std::bind(count, std::cref(polygons), _1, _2);
-  commands["MAX"] = std::bind(max, std::cref(polygons), _1, _2);
-  commands["MIN"] = std::bind(min, std::cref(polygons), _1, _2);
-  commands["INFRAME"] = std::bind(inframe, std::cref(polygons), _1, _2);
-  commands["ECHO"] = std::bind(echo, std::ref(polygons), _1, _2);
-  commands["RIGHTSHAPES"] = std::bind(rightshapes, std::cref(polygons), _2);
-  commands["LESSAREA"] = std::bind(lessArea, std::cref(polygons), _1, _2);
-  commands["INTERSECTIONS"] = std::bind(intersections, std::cref(polygons), _1, _2);
-  return commands;
+  CommandMap createCommandMap(std::vector<Polygon>& polygons)
+  {
+    CommandMap commands;
+    using namespace std::placeholders;
+    commands["AREA"] = std::bind(area, std::cref(polygons), _1, _2);
+    commands["COUNT"] = std::bind(count, std::cref(polygons), _1, _2);
+    commands["MAX"] = std::bind(max, std::cref(polygons), _1, _2);
+    commands["MIN"] = std::bind(min, std::cref(polygons), _1, _2);
+    commands["INFRAME"] = std::bind(inframe, std::cref(polygons), _1, _2);
+    commands["ECHO"] = std::bind(echo, std::ref(polygons), _1, _2);
+    commands["RIGHTSHAPES"] = std::bind(rightshapes, std::cref(polygons), _2);
+    commands["LESSAREA"] = std::bind(lessArea, std::cref(polygons), _1, _2);
+    commands["INTERSECTIONS"] = std::bind(intersections, std::cref(polygons), _1, _2);
+    return commands;
+  }
 }
