7d2563aeb3591757dba7d0c4c0480848d9ed5d56
temaPop1e <popov.artoym7@gmail.com>
2025-06-06 22:47:23 +0300

    Update polygon_operations.hpp123

--- popov.art/T3/polygon_operations.hpp
+++ popov.art/T3/polygon_operations.hpp
@@ -5,14 +5,14 @@
 #include <iostream>
 namespace popov
 {
-  void area(const std::vector< Polygon >& value, std::istream& in, std::ostream& out);
-  void max(const std::vector< Polygon >& value, std::istream& in, std::ostream& out);
-  void min(const std::vector< Polygon >& value, std::istream& in, std::ostream& out);
-  void count(const std::vector< Polygon >& value, std::istream& in, std::ostream& out);
-  void rightshapes(const std::vector< Polygon >& value, std::ostream& out);
-  void echo(std::vector< Polygon >& value, std::istream& in, std::ostream& out);
-  void inframe(const std::vector< Polygon >& value, std::istream& in, std::ostream& out);
-  void lessArea(const std::vector< Polygon >& value, std::istream& in, std::ostream& out);
-  void intersections(const std::vector< Polygon >& value, std::istream& in, std::ostream& out);
+  void area(const std::vector<Polygon>& value, std::istream& in, std::ostream& out);
+  void max(const std::vector<Polygon>& value, std::istream& in, std::ostream& out);
+  void min(const std::vector<Polygon>& value, std::istream& in, std::ostream& out);
+  void count(const std::vector<Polygon>& value, std::istream& in, std::ostream& out);
+  void rightshapes(const std::vector<Polygon>& value, std::ostream& out);
+  void echo(std::vector<Polygon>& value, std::istream& in, std::ostream& out);
+  void inframe(const std::vector<Polygon>& value, std::istream& in, std::ostream& out);
+  void lessArea(const std::vector<Polygon>& value, std::istream& in, std::ostream& out);
+  void intersections(const std::vector<Polygon>& value, std::istream& in, std::ostream& out);
 }
 #endif
