8bdb54923582d1b74e406cb342560867c7757b99
temaPop1e <popov.artoym7@gmail.com>
2025-06-06 21:47:57 +0300

    Update geometry.h13

--- popov.art/T3/geometry.h
+++ popov.art/T3/geometry.h
@@ -1,17 +1,51 @@
-#ifndef GEOMETRY_H
-#define GEOMETRY_H
-
+#ifndef GEOMETRY_HPP
+#define GEOMETRY_HPP
+#include <iostream>
 #include <vector>
-#include <string>
-
-struct Point {
-    int x, y;
-    bool operator==(const Point& other) const;
-};
-
-struct Polygon {
-    std::vector<Point> points;
-    bool operator==(const Polygon& other) const;
-};
-
-#endif // GEOMETRY_H
+#include <functional>
+namespace popov
+{
+  struct DelimiterChar
+  {
+    char expected;
+  };
+  std::istream & operator>>(std::istream& in, DelimiterChar&& exp);
+  struct DelimiterString
+  {
+    const char * expected;
+  };
+  std::istream& operator>>(std::istream& in, DelimiterString&& exp);
+  struct Point
+  {
+    int x;
+    int y;
+  };
+  std::istream& operator>>(std::istream& in, Point& point);
+  bool operator==(const Point& lhs, const Point& rhs);
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+  double getPolygonArea(const Polygon& polygon);
+  std::istream& operator>>(std::istream& in, Polygon& polygon);
+  bool operator==(const Polygon& lhs, const Polygon& rhs);
+  bool operator<=(const Polygon& lhs, const Polygon& rhs);
+  bool isRightAngle(const Polygon& polygon);
+  int findMaxX(const Polygon& polygon);
+  int findMaxY(const Polygon& polygon);
+  int findMinX(const Polygon& polygon);
+  int findMinY(const Polygon& polygon);
+  Polygon getBoundingBox(const std::vector< Polygon >& polygon);
+  struct AreaPolygon
+  {
+    Point p1;
+    double operator()(double area, const Point& p2, const Point& p3);
+  };
+  struct accumulateRightAngle
+  {
+    Point p1;
+    Point p2;
+    bool operator()(const Point& p3);
+  };
+}
+#endif
