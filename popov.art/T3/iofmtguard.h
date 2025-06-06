6b84c42c3b7c0a8d2fb2fe3f6456fee471595329
temaPop1e <popov.artoym7@gmail.com>
2025-06-06 21:51:00 +0300

    Create iofmtguard.h1

---
+++ popov.art/T3/iofmtguard.h
@@ -0,0 +1,17 @@
+#ifndef IOFMTGUARD_HPP
+#define IOFMTGUARD_HPP
+#include <ios>
+namespace popov
+{
+  class iofmtguard
+  {
+  public:
+    explicit iofmtguard(std::basic_ios<char>& s);
+    ~iofmtguard();
+  private:
+    std::basic_ios<char>& s_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags flags_;
+  };
+}
+#endif
