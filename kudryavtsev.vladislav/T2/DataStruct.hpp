b87150b7db4d6e9ac766277f32c395d681b86ee3
Vladislav <vldkudryavtsev@gmail.com>
2025-06-06 22:23:40 +0300

    fixed T2

---
+++ kudryavtsev.vladislav/T2/DataStruct.hpp
@@ -0,0 +1,18 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <complex>
+
+namespace myspace {
+    struct DataStruct {
+        std::complex<double> key1;
+        std::string key2;
+        std::string key3;
+
+        friend std::ostream& operator<<(std::ostream&, const DataStruct&);
+        friend std::istream& operator>>(std::istream&, DataStruct&);
+    };
+    bool compare(const DataStruct&, const DataStruct&);
+}
+
+#endif
---
+++ kudryavtsev.vladislav/T2/main.cpp
@@ -0,0 +1,24 @@
+//CMP LSP and ULL BIN//
+
+#include "DataStruct.hpp"
+
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <limits>
+
+using myspace::DataStruct;
+
+int main() {
+    std::vector<DataStruct> ds;
+    while(!std::cin.eof()) {
+        std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(ds));
+        if(!std::cin.fail()) continue;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    std::sort(ds.begin(), ds.end(), myspace::compare);
+    std::copy(ds.begin(), ds.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+    return 0;
+}
