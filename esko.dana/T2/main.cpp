1d259454b9b575afa058324d6ca868389eea8bb4
Dana Esko <danaesko12@gmail.com>
2025-06-07 02:08:50 +0300

    Добавлен main.cpp для T2

--- esko.dana/T2/main.cpp
+++ esko.dana/T2/main.cpp
@@ -6,104 +6,79 @@
 #include <windows.h>
 
 struct DataStruct {
-    double      key1;   // число с плавающей точкой (double), формат —         :key1 30.0d:
-    long long   key2;   // целое знаковое число (long long), формат —          :key2 -89LL:
-    std::string key3;   // строка в кавычках, может содержать спец. символы —  :key3 "Data":
+    double      key1;
+    long long   key2;
+    std::string key3;
 };
 
-bool parse(std::string const & str, DataStruct & o) {   // парсинг и заполнение структуры о
-    if (str[0] != '(')        // проверка скобки
+bool parse(std::string const & str, DataStruct & o) {
+    if (str[0] != '(')
         return false;
 
-    size_t m = 0;             // флаг чтобы не допустить повторного появления одного и того же поля
+    size_t m = 0;
     for (size_t i = 1; i < str.size(); ++i) {
         if (str[i] == ':') {
             std::string key = "";
             for (i = i + 1; i < str.size() && (str[i] != ' ' && str[i] != ')'); ++i)
                 key += str[i];
-
             if (str[i] == ')' && key.empty()) {
                 if (i == str.size() - 1 && m == 7)
                     return true;
-
                 return false;
             }
-
             if (key == "key1") {
                 if (m & 1)
                     return false;
                 m |= 1;
-
                 std::string value = "";
                 for (i = i + 1; i < str.size() && (str[i] != 'D' && str[i] != 'd'); ++i)
                     value += str[i];
-
                 if (value.empty())
                     return false;
-
                 if (value.front() == ' ')
                     return false;
-
                 if (value.back()  == ' ')
                     return false;
-
                 o.key1 = std::stod(value);
-
                 continue;
             }
-
             if (key == "key2") {
                 if (m & 2)
                     return false;
                 m |= 2;
-
                 std::string value = "";
                 for (i = i + 1; i < str.size() && (str[i] != 'L' && str[i] != 'l'); ++i)
                     value += str[i];
-
                 if (value.empty())
                     return false;
-
                 if (value.front() == ' ')
                     return false;
-
                 if (value.back()  == ' ')
                     return false;
-
                 o.key2 = std::stoll(value);
-
                 if (++i == str.size())
                     return false;
-
                 if (str[i] != 'L' && str[i] != 'l')
                     return false;
-
                 continue;
             }
-
             if (key == "key3") {
                 if (m & 4)
                     return false;
                 m |= 4;
-
                 if (++i == str.size())
                     return false;
-
                 if (str[i] != '"')
                     return false;
-
                 std::string value = "";
                 for (i = i + 1; i < str.size(); ++i) {
                     if (str[i] == '"' && str[i - 1] != '\\')
                         break;
                     value += str[i];
                 }
-
                 o.key3 = value;
-
                 continue;
             }
-
             return false;
         }
     }
@@ -124,7 +99,7 @@ std::istream& operator>>(std::istream& in, DataStruct& o) {
     return in;
 }
 
-std::ostream& operator<<(std::ostream & out, DataStruct const & o) {        // перегрузка оператора вывода
+std::ostream& operator<<(std::ostream & out, DataStruct const & o) {
     out << "(:key1" << " " << std::fixed << std::setprecision(1) << o.key1 << "D"
         <<  ":key2" << " " << o.key2 << "LL"
         <<  ":key3" << " " << "\"" << o.key3 << "\":)";
