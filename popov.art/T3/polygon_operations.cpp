469bcca21f86e551e7940f7a268dbfe03b0c8691
temaPop1e <popov.artoym7@gmail.com>
2025-06-07 00:24:47 +0300

    Update polygon_operations.cpp

--- popov.art/T3/polygon_operations.cpp
+++ popov.art/T3/polygon_operations.cpp
@@ -218,14 +218,9 @@ void popov::echo(std::vector<Polygon>& value, std::istream& in, std::ostream& ou
     {
         throw std::invalid_argument("<INVALID COMMAND>");
     }
-    size_t count = 0;
-    for (const auto& p : value) {
-        if (p == polygon) {
-            count++;
-        }
-    }
-    for (size_t i = 0; i < count; ++i) {
-        value.push_back(polygon);
+    size_t count = std::count(value.begin(), value.end(), polygon);
+    if (count > 0) {
+        value.insert(value.end(), count, polygon);
     }
     out << count;
 }
