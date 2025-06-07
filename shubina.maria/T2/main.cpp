0494bb06aa6b7611b2ff673b9a66539201e1f34f
Maria-Mariaa <sm4riia@yandex.ru>
2025-06-07 21:36:41 +0300

    t3

--- shubina.maria/T2/main.cpp
+++ shubina.maria/T2/main.cpp
@@ -15,7 +15,7 @@ int main() {
     );
 
     if (dataVector.empty()) {
-        std::cout << "Atleast one supported record type\n";
+        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
         return 0;
     }
 
