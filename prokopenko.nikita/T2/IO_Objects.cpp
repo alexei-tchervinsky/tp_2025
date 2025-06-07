ae4345471908da96f5f2d70d9577a16ddb51aec0
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2025-06-07 15:02:15 +0300

    Fix: исправлены ошибки в IO_Objects.cpp для T2

--- prokopenko.nikita/T2/IO_Objects.cpp
+++ prokopenko.nikita/T2/IO_Objects.cpp
@@ -1,60 +1,63 @@
 #include "IO_Objects.hpp"
-
-#include <sstream>
+#include "iofmtguard.hpp"
+#include <iostream>
 #include <iomanip>
+#include <ios>
 
-namespace ponomarenko {
+namespace prokopenko {
 
-    std::istream& operator>>(std::istream& in, DelimiterIO&& d) {
-        char ch;
-        in >> std::ws >> ch;
-        if (!in || ch != d.expected) in.setstate(std::ios::failbit);
-        return in;
+  std::istream& operator>>(std::istream& in, CharIO&& c) {
+    char quote1 = 0, quote2 = 0;
+    char value;
+
+    if (in >> quote1 >> value >> quote2) {
+      if (in && quote1 == '\'' && quote2 == '\'') {
+        c.ref = value;
+      }
+      else {
+        in.setstate(std::ios::failbit);
+      }
     }
 
-    std::istream& operator>>(std::istream& in, LabelIO&& label) {
-        std::string result;
-        char ch;
-        while (in.get(ch)) {
-            if (ch == ' ') break;
-            result += ch;
-        }
-        if (in) label.ref = result;
-        else in.setstate(std::ios::failbit);
-        return in;
+    return in;
+  }
+
+  std::ostream& operator<<(std::ostream& out, const CharIO& c) {
+    iofmtguard guard(out);
+    out << '\'' << c.ref << '\'';
+    return out;
+  }
+
+  std::istream& operator>>(std::istream& in, DataStruct& ds) {
+    char c = 0;
+    if (!(in >> c) || c != '(') {
+      in.setstate(std::ios::failbit);
+      return in;
     }
 
-    std::istream& operator>>(std::istream& in, ULLLitIO&& u) {
-        std::string str;
-        in >> str;
-        if (str.size() > 3 && (str.substr(str.size()-3) == "ull" || str.substr(str.size()-3) == "ULL")) {
-            str = str.substr(0, str.size()-3);
-            std::istringstream ss(str);
-            ss >> u.ref;
-            if (!ss) in.setstate(std::ios::failbit);
-        } else {
-            in.setstate(std::ios::failbit);
-        }
-        return in;
+    CharIO keyIO{ ds.key };
+    CharIO chIO{ ds.character };
+    CharIO valueIO{ ds.value };
+
+    if (!(in >> keyIO >> chIO >> valueIO)) {
+      return in;
     }
 
-    std::istream& operator>>(std::istream& in, CharIO&& c) {
-        char quote1, value, quote2;
-        in >> quote1 >> value >> quote2;
-        if (in && quote1 == ''' && quote2 == ''') c.ref = value;
-        else in.setstate(std::ios::failbit);
-        return in;
+    if (!(in >> c) || c != ')') {
+      in.setstate(std::ios::failbit);
+      return in;
     }
 
-    std::istream& operator>>(std::istream& in, StringIO&& s) {
-        char quote;
-        in >> std::ws >> quote;
-        if (quote != '"') {
-            in.setstate(std::ios::failbit);
-            return in;
-        }
-        std::getline(in, s.ref, '"');
-        if (!in) in.setstate(std::ios::failbit);
-        return in;
-    }
-}
+    return in;
+  }
+
+  std::ostream& operator<<(std::ostream& out, const DataStruct& ds) {
+    iofmtguard guard(out);
+    out << "("
+      << CharIO{ ds.key } << " "
+      << CharIO{ ds.character } << " "
+      << CharIO{ ds.value } << ")";
+    return out;
+  }
+
+} // namespace prokopenko
