eafa202290af780d2000e5db74279bebe7ebae01
senku812 <xaomi29122017@gmail.com>
2025-06-07 19:35:56 +0300

    Update main.cpp

--- koshelev.yaroslav/T2/main.cpp
+++ koshelev.yaroslav/T2/main.cpp
@@ -2,33 +2,25 @@
 #include "IO_Objects.h"
 #include <vector>
 #include <algorithm>
-#include <iterator>
 #include <iostream>
 
 int main() {
     std::vector<DataStruct> data;
+    DataStruct temp;
 
-    try {
-        std::copy(
-            std::istream_iterator<DataStruct>(std::cin),
-            std::istream_iterator<DataStruct>(),
-            std::back_inserter(data)
-        );
-
-        if (data.empty()) {
-            std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
-            return 0;
+    while (std::cin >> temp) {
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
         }
+        data.push_back(temp);
+    }
 
-        std::sort(data.begin(), data.end(), compareDataStruct);
+    std::sort(data.begin(), data.end(), compareDataStruct);
 
-        std::copy(
-            data.begin(),
-            data.end(),
-            std::ostream_iterator<DataStruct>(std::cout, "\n")
-        );
-    } catch (...) {
-        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
+    for (const auto& item : data) {
+        std::cout << item << '\n';
     }
 
     return 0;
