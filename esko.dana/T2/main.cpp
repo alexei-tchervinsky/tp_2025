6cb7aec8db6c6e078408c4bef5b8a3c376e8cc30
Dana Esko <danaesko12@gmail.com>
2025-06-07 14:20:19 +0300

    Исправлен вывод

--- esko.dana/T2/main.cpp
+++ esko.dana/T2/main.cpp
@@ -18,91 +18,7 @@ void skipSpaces(const std::string& str, size_t& i) {
     while (i < str.size() && std::isspace(str[i])) ++i;
 }
 
-bool parse(const std::string& str, DataStruct& o) {
-    if (str.empty() || str.front() != '(' || str.back() != ')')
-        return false;
-
-    size_t i = 1;
-    int flags = 0;
-
-    while (i < str.size()) {
-        skipSpaces(str, i);
-        if (i >= str.size() || str[i] != ':') break;
-        ++i;
-
-        std::string key;
-        while (i < str.size() && std::isalpha(str[i]))
-            key += str[i++];
-
-        skipSpaces(str, i);
-
-        if (key == "key1") {
-            if (flags & 1) return false;
-            flags |= 1;
-
-            std::string val;
-            while (i < str.size()
-                && (std::isdigit(str[i]) ||
-                    str[i] == '.' ||
-                    str[i] == '-' ||
-                    str[i] == 'e' ||
-                    str[i] == '+' ||
-                    str[i] == 'E'))
-                val += str[i++];
-
-            if (i >= str.size() || (str[i] != 'D' && str[i] != 'd')) return false;
-            ++i;
-
-            try {
-                o.key1 = std::stod(val);
-            } catch (...) {
-                return false;
-            }
-        }
-        else if (key == "key2") {
-            if (flags & 2) return false;
-            flags |= 2;
-
-            std::string val;
-            while (i < str.size() && (std::isdigit(str[i]) || str[i] == '-'))
-                val += str[i++];
-
-            if (i + 1 >= str.size()) return false;
-            if (!((str[i] == 'L' || str[i] == 'l') && (str[i + 1] == 'L' || str[i + 1] == 'l')))
-                return false;
-            i += 2;
-
-            try {
-                o.key2 = std::stoll(val);
-            } catch (...) {
-                return false;
-            }
-        }
-        else if (key == "key3") {
-            if (flags & 4) return false;
-            flags |= 4;
-
-            if (i >= str.size() || str[i] != '"') return false;
-            ++i;
-
-            std::string val;
-            while (i < str.size()) {
-                if (str[i] == '"' && (i == 0 || str[i - 1] != '\\')) break;
-                val += str[i++];
-            }
-
-            if (i >= str.size() || str[i] != '"') return false;
-            ++i;
-
-            o.key3 = val;
-        }
-        else {
-            return false;
-        }
-    }
-
-    return (flags == 7);
-}
+bool parse(const std::string& str, DataStruct& o);
 
 std::istream& operator>>(std::istream& in, DataStruct& o) {
     std::string line;
@@ -147,4 +63,90 @@ int main() {
     std::cout << std::endl;
 
     return 0;
+}
+
+bool parse(const std::string& str, DataStruct& o) {
+    if (str.empty() || str.front() != '(' || str.back() != ')')
+        return false;
+
+    size_t i = 1;
+    int flags = 0;
+
+    while (i < str.size()) {
+
+        if (str[i] != ':') break;
+        ++i;
+
+        std::string key;
+        while (i < str.size() && std::isalpha(str[i]))
+            key += str[i++];
+
+        if (key.empty()) return false;
+
+        if (key == "key1") {
+            if (flags & 1) return false;
+            flags |= 1;
+
+            std::string val;
+            while (i < str.size() && str[i] != ':' && str[i] != ')')
+                val += str[i++];
+
+            if (val.empty() || (val.back() != 'D' && val.back() != 'd'))
+                return false;
+
+            val.pop_back();
+            try {
+                o.key1 = std::stod(val);
+            } catch (...) {
+                return false;
+            }
+        }
+        else if (key == "key2") {
+            if (flags & 2) return false;
+            flags |= 2;
+
+            std::string val;
+            while (i < str.size() && str[i] != ':' && str[i] != ')')
+                val += str[i++];
+
+            if (val.size() < 2) return false;
+
+            std::string suffix = val.substr(val.size() - 2);
+            for (auto& c : suffix) c = std::toupper(c);
+            if (suffix != "LL") return false;
+
+            val.erase(val.size() - 2);
+
+            try {
+                o.key2 = std::stoll(val);
+            } catch (...) {
+                return false;
+            }
+        }
+        else if (key == "key3") {
+            if (flags & 4) return false;
+            flags |= 4;
+
+            if (i >= str.size() || str[i] != '"') return false;
+            ++i;
+
+            std::string val;
+            while (i < str.size()) {
+                if (str[i] == '"' && (i == 0 || str[i - 1] != '\\'))
+                    break;
+                val += str[i++];
+            }
+
+            if (i >= str.size() || str[i] != '"') return false;
+            ++i;
+
+            o.key3 = val;
+        }
+        else {
+
+            return false;
+        }
+    }
+
+    return (flags == 7);
 }
