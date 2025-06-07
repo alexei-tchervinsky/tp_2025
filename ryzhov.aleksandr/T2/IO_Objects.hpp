bb33033a3aff8df8af851d24ca7ba6a447ded9cc
Ryzhov Aleksandr <lasimerunes@mail.ru>
2025-06-07 12:17:11 +0300

    T2 commit

---
+++ ryzhov.aleksandr/T2/IO_Objects.hpp
@@ -0,0 +1,36 @@
+#ifndef IO_OBJECTS_HPP
+#define IO_OBJECTS_HPP
+
+#include <complex>
+#include <iostream>
+
+
+namespace ryzhov {
+    struct DelimiterIO {
+        char delimiter;
+    };
+
+    struct ComplexDoubleIO {
+        std::complex<double> &ref;
+    };
+
+    struct StringIO {
+        std::string &ref;
+    };
+
+    struct SignedLongLongIO {
+        long long &ref;
+    };
+
+    struct LabelIO {
+        std::string label;
+    };
+
+    std::istream &operator>>(std::istream &, LabelIO &&);
+    std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
+    std::istream &operator>>(std::istream &in, ComplexDoubleIO &&dest);
+    std::istream &operator>>(std::istream &in, SignedLongLongIO &&dest);
+    std::istream &operator>>(std::istream &in, StringIO &&dest);
+}
+
+#endif //IO_OBJECTS_HPP
