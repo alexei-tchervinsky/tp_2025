4330d55cb1b59c40fc89e562227f086c3eff5225
senku812 <xaomi29122017@gmail.com>
2025-06-07 19:33:15 +0300

    Update DataStruct.h

--- koshelev.yaroslav/T2/DataStruct.h
+++ koshelev.yaroslav/T2/DataStruct.h
@@ -1,15 +1,21 @@
 #ifndef DATASTRUCT_H
 #define DATASTRUCT_H
 
+#include <complex>
 #include <string>
+#include <utility>
+#include <iosfwd>
 
 struct DataStruct {
-    double key1;
-    unsigned long long key2;
+    std::complex<double> key1;
+    std::pair<long long, unsigned long long> key2;
     std::string key3;
 };
 
-bool parseScientificDouble(const std::string& str, double& value);
-bool parseHexULL(const std::string& str, unsigned long long& value);
+bool parseComplex(const std::string& str, std::complex<double>& value);
+bool parseRational(const std::string& str, std::pair<long long, unsigned long long>& value);
+std::istream& operator>>(std::istream& in, DataStruct& data);
+std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+bool compareDataStruct(const DataStruct& a, const DataStruct& b);
 
 #endif
