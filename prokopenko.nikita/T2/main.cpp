5562f3d43e256a43aea295b2fa950bc06f920b4c
Никита <nikita.prokopenko.kz@gmail.com>
2025-06-07 18:05:40 +0300

    Update main.cpp

--- prokopenko.nikita/T2/main.cpp
+++ prokopenko.nikita/T2/main.cpp
@@ -4,28 +4,20 @@
 #include <limits>
 #include "DataStruct.hpp"
 
-// Главная функция
 int main() {
     std::vector<prokopenko::DataStruct> data;
     prokopenko::DataStruct temp;
-
-    // Считываем данные до конца ввода
     while (!std::cin.eof()) {
         if (std::cin >> temp) {
-            data.push_back(temp);  // Успешное чтение — добавляем в вектор
+            data.push_back(temp);
         } else {
-            std::cin.clear();  // Сброс ошибки
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Пропуск до конца строки
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
-
-    // Сортируем структуру по заданному критерию
     std::sort(data.begin(), data.end());
-
-    // Выводим отсортированные данные
     for (const auto& el : data) {
         std::cout << el << "\n";
     }
-
     return 0;
 }
