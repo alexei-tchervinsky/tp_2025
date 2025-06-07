05cd62ba8f39f0e36e721f89bfb20bf88bebc11a
temaPop1e <popov.artoym7@gmail.com>
2025-06-07 03:04:05 +0300

    Update polygon_operations.h

--- popov.art/T3/polygon_operations.h
+++ popov.art/T3/polygon_operations.h
@@ -1,16 +1,12 @@
 #ifndef POLYGON_OPERATIONS_H
 #define POLYGON_OPERATIONS_H
-
 #include "geometry.h"
 #include <vector>
 #include <sstream>
-
-
 Polygon parsePolygon(const std::string& input) {
     std::istringstream iss(input);
     int vertexCount;
     iss >> vertexCount;
-    
     Polygon poly;
     for (int i = 0; i < vertexCount; ++i) {
         char c1, c2, c3;
@@ -20,9 +16,7 @@ Polygon parsePolygon(const std::string& input) {
     }
     return poly;
 }
-
 double calculateArea(const Polygon& poly);
 bool isPolygonInFrame(const Polygon& poly, const std::vector<Polygon>& polygons);
 bool doPolygonsIntersect(const Polygon& a, const Polygon& b);
-
 #endif // POLYGON_OPERATIONS_H
