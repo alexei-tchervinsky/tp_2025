1d83ac4253bcbe0db21ea1f4883d22f40c60bdef
senku812 <xaomi29122017@gmail.com>
2025-06-07 18:28:44 +0300

    Update DataStruct.cpp

--- koshelev.yaroslav/T2/DataStruct.cpp
+++ koshelev.yaroslav/T2/DataStruct.cpp
@@ -1,20 +1,49 @@
 #include "DataStruct.h"
 #include <sstream>
-#include <cctype>
+#include <cmath>
 
-bool parseScientificDouble(const std::string& str, double& value) {
-    std::istringstream iss(str);
-    iss >> value;
-    return !iss.fail() && iss.eof();
-}
-
-bool parseHexULL(const std::string& str, unsigned long long& value) {
-    std::string s = str;
-    if (s.size() > 1 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) {
-        s = s.substr(2);
+bool parseComplex(const std::string& str, std::complex<double>& value) {
+    if (str.size() < 5 || str.substr(0, 3) != "#c(" || str.back() != ')') {
+        return false;
     }
     
-    std::istringstream iss(s);
-    iss >> std::hex >> value;
-    return !iss.fail() && iss.eof();
+    std::istringstream iss(str.substr(3, str.size() - 4));
+    double real, imag;
+    if (!(iss >> real >> imag)) {
+        return false;
+    }
+    
+    value = std::complex<double>(real, imag);
+    return true;
+}
+
+bool parseRational(const std::string& str, std::pair<long long, unsigned long long>& value) {
+    if (str.size() < 7 || str.substr(0, 2) != "(:") {
+        return false;
+    }
+    
+    size_t end = str.rfind(":)");
+    if (end == std::string::npos) {
+        return false;
+    }
+    
+    std::istringstream iss(str.substr(2, end - 2));
+    std::string token;
+    bool hasN = false, hasD = false;
+    
+    while (iss >> token) {
+        if (token == ":N") {
+            if (!(iss >> value.first)) {
+                return false;
+            }
+            hasN = true;
+        } else if (token == ":D") {
+            if (!(iss >> value.second)) {
+                return false;
+            }
+            hasD = true;
+        }
+    }
+    
+    return hasN && hasD;
 }
