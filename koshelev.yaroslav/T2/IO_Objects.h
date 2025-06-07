0592fcc00cead5593f7fd05f08e62aa5bdf6a9ec
senku812 <xaomi29122017@gmail.com>
2025-06-07 19:33:32 +0300

    Update IO_Objects.h

--- koshelev.yaroslav/T2/IO_Objects.h
+++ koshelev.yaroslav/T2/IO_Objects.h
@@ -1,8 +1,8 @@
 #ifndef IO_OBJECTS_H
 #define IO_OBJECTS_H
 
+#include <iosfwd>
 #include "DataStruct.h"
-#include <iostream>
 
 std::istream& operator>>(std::istream& in, DataStruct& data);
 std::ostream& operator<<(std::ostream& out, const DataStruct& data);
