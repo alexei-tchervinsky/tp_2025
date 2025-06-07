8724b0ea35eb702887af597973514523e661caec
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2025-06-07 10:51:07 +0300

    Исправлена проверка кавычек в операторе ввода CharIO для корректного парсинга символов

--- prokopenko.nikita/T2/IO_Objects.cpp
+++ prokopenko.nikita/T2/IO_Objects.cpp
@@ -1,81 +1,80 @@
 #include "IO_Objects.hpp"
+
 #include <cctype>
 #include <sstream>
 #include <iomanip>
 
 namespace prokopenko {
 
-    // Чтение символа-разделителя
-    std::istream& operator>>(std::istream& in, DelimiterIO&& d) {
-        char ch;
-        in >> std::ws >> ch;
-        if (!in || ch != d.expected) {
-            in.setstate(std::ios::failbit);
-        }
-        return in;
+  std::istream& operator>>(std::istream& in, DelimiterIO&& d) {
+    char ch;
+    in >> std::ws >> ch;
+    if (!in || ch != d.expected) {
+      in.setstate(std::ios::failbit);
     }
+    return in;
+  }
 
-    // Чтение метки ключа
-    std::istream& operator>>(std::istream& in, LabelIO&& label) {
-        std::string result;
-        char ch;
-        while (in.get(ch)) {
-            if (ch == ' ') {
-                break;
-            }
-            result += ch;
-        }
-        if (in) {
-            label.ref = result;
-        } else {
-            in.setstate(std::ios::failbit);
-        }
-        return in;
+  std::istream& operator>>(std::istream& in, LabelIO&& label) {
+    std::string result;
+    char ch;
+    while (in.get(ch)) {
+      if (ch == ' ') {
+        break;
+      }
+      result += ch;
     }
+    if (in) {
+      label.ref = result;
+    }
+    else {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
 
-    // Чтение комплексного числа
-    std::istream& operator>>(std::istream& in, ComplexIO&& c) {
-        char hash, cM, open, close;
-        double real = 0.0, imag = 0.0;
-        in >> hash >> cM >> open;
-        if (hash != '#' || cM != 'c' || open != '(') {
-            in.setstate(std::ios::failbit);
-            return in;
-        }
-        in >> real >> imag >> close;
-        if (!in || close != ')') {
-            in.setstate(std::ios::failbit);
-            return in;
-        }
-        c.ref = std::complex<double>(real, imag);
-        return in;
+  std::istream& operator>>(std::istream& in, ComplexIO&& c) {
+    char hash, cM, open, close;
+    double real = 0.0, imag = 0.0;
+    in >> hash >> cM >> open;
+    if (hash != '#' || cM != 'c' || open != '(') {
+      in.setstate(std::ios::failbit);
+      return in;
     }
+    in >> real >> imag >> close;
+    if (!in || close != ')') {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    c.ref = std::complex<double>(real, imag);
+    return in;
+  }
 
-    // Чтение символа
-    std::istream& operator>>(std::istream& in, CharIO&& c) {
-        char quote1, value, quote2;
-        in >> quote1 >> value >> quote2;
-        if (in && quote1 == ''' && quote2 == ''') {
-            c.ref = value;
-        } else {
-            in.setstate(std::ios::failbit);
-        }
-        return in;
+  std::istream& operator>>(std::istream& in, CharIO&& c) {
+    char quote1, value, quote2;
+    in >> quote1 >> value >> quote2;
+    // Исправленная проверка одиночных кавычек
+    if (in && quote1 == '\'' && quote2 == '\'') {
+      c.ref = value;
     }
+    else {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
 
-    // Чтение строки
-    std::istream& operator>>(std::istream& in, StringIO&& s) {
-        char quote;
-        in >> std::ws >> quote;
-        if (quote != '"') {
-            in.setstate(std::ios::failbit);
-            return in;
-        }
-        std::getline(in, s.ref, '"');
-        if (!in) {
-            in.setstate(std::ios::failbit);
-        }
-        return in;
+  std::istream& operator>>(std::istream& in, StringIO&& s) {
+    char quote;
+    in >> std::ws >> quote;
+    if (quote != '"') {
+      in.setstate(std::ios::failbit);
+      return in;
     }
+    std::getline(in, s.ref, '"');
+    if (!in) {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
 
 }
