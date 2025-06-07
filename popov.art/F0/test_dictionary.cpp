ebc139d0f11eab2e4005513ae843e328d05600bf
temaPop1e <popov.artoym7@gmail.com>
2025-06-07 03:16:06 +0300

    Update test_dictionary.cpp

--- popov.art/F0/test_dictionary.cpp
+++ popov.art/F0/test_dictionary.cpp
@@ -1,7 +1,6 @@
 #include "dictionary.hpp"
 #include <iostream>
 #include <cassert>
-
 void test_insert_search() {
     DictionaryHashTable dict(10);
     dict.insert("apple", "яблоко");
@@ -9,14 +8,12 @@ void test_insert_search() {
     assert(entry != nullptr);
     assert(entry->getKey() == "apple");
 }
-
 void test_remove() {
     DictionaryHashTable dict(10);
     dict.insert("car", "машина");
     assert(dict.remove("car"));
     assert(!dict.search("car"));
 }
-
 void runAllTests() {
     test_insert_search();
     test_remove();
