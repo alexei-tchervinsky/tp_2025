f30a0221f06974f1a5fad156c198a7318bdbdb02
Тарасенко Никита Денисович <niktarasenko26@yandex.ru>
2025-06-07 16:19:53 +0300

    Initial commit for T3

--- tarasenko.nikita/T3/commands.cpp
+++ tarasenko.nikita/T3/commands.cpp
@@ -14,7 +14,6 @@ namespace tarasenko {
         }
 
         bool doPolygonsIntersect(const Polygon& poly1, const Polygon& poly2) {
-            // Check if any edges intersect
             bool edgesIntersect = std::any_of(poly1.points.begin(), poly1.points.end(),
                 [&poly1, &poly2](const Point& p1) {
                     size_t i = &p1 - &poly1.points[0];
@@ -43,7 +42,6 @@ namespace tarasenko {
 
             if (edgesIntersect) return true;
 
-            // Check if one polygon is completely inside another
             auto isPointInside = [](const Point& p, const Polygon& poly) -> bool {
                 if (poly.points.empty()) return false;
 
