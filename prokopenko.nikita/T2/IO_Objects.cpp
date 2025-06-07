5ac441067fa41a51c760cd1331a072749b6eeb33
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2025-06-07 15:18:10 +0300

    Fix compilation errors in IO_Objects.cpp

--- prokopenko.nikita/T2/IO_Objects.cpp
+++ prokopenko.nikita/T2/IO_Objects.cpp
@@ -1,52 +1,51 @@
 #include "IO_Objects.hpp"
 #include "iofmtguard.hpp"
-#include "DataStruct.hpp"
 #include <iostream>
 
 namespace prokopenko {
 
-std::istream& operator>>(std::istream& in, CharIO&& c) {
+  // ???????? ????? ??? CharIO (?????????? c ?????????? ?? ??????)
+  std::istream& operator>>(std::istream& in, CharIO& c) {
     char ch;
     in >> ch;
-    if (ch != ''') {
-        in.setstate(std::ios::failbit);
-        return in;
+    if (ch != '\'') {
+      in.setstate(std::ios::failbit);
+      return in;
     }
-
     in >> c.ref;
-
     in >> ch;
-    if (ch != ''') {
-        in.setstate(std::ios::failbit);
-        return in;
+    if (ch != '\'') {
+      in.setstate(std::ios::failbit);
+      return in;
     }
-
     return in;
-}
+  }
 
-std::ostream& operator<<(std::ostream& out, const CharIO& c) {
+  // ???????? ?????? ??? CharIO
+  std::ostream& operator<<(std::ostream& out, const CharIO& c) {
     iofmtguard guard(out);
-    return out << ''' << c.ref << ''';
-}
+    return out << '\'' << c.ref << '\'';
+  }
 
-std::istream& operator>>(std::istream& in, DataStruct& ds) {
+  // ???????? ????? ??? DataStruct (??????, ?????? ?? ???? ????)
+  std::istream& operator>>(std::istream& in, DataStruct& ds) {
     CharIO keyIO{ ds.key };
     CharIO chIO{ ds.character };
     CharIO valueIO{ ds.value };
 
     if (!(in >> keyIO >> chIO >> valueIO)) {
-        in.setstate(std::ios::failbit);
+      return in;
     }
-
     return in;
-}
+  }
 
-std::ostream& operator<<(std::ostream& out, const DataStruct& ds) {
+  // ???????? ?????? ??? DataStruct
+  std::ostream& operator<<(std::ostream& out, const DataStruct& ds) {
     iofmtguard guard(out);
     return out << "("
-               << CharIO{ ds.key } << " "
-               << CharIO{ ds.character } << " "
-               << CharIO{ ds.value } << ")";
-}
+      << CharIO{ ds.key } << " "
+      << CharIO{ ds.character } << " "
+      << CharIO{ ds.value } << ")";
+  }
 
 } // namespace prokopenko
