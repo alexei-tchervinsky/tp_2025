e1fd5bfce42283965f820560b5bccd1005710419
Dana Esko <danaesko12@gmail.com>
2025-06-07 14:59:21 +0300

    изменен main

--- esko.dana/T2/main.cpp
+++ esko.dana/T2/main.cpp
@@ -3,173 +3,165 @@
 #include <iterator>
 #include <algorithm>
 #include <iomanip>
-#include <string>
-#include <sstream>
-#include <stdexcept>
-#include <cctype>
 
 struct DataStruct {
-    double key1;
-    long long key2;
+    double      key1;
+    long long   key2;
     std::string key3;
 };
 
-void skipSpaces(const std::string& str, size_t& i) {
-    while (i < str.size() && std::isspace(str[i])) ++i;
-}
-
-bool parseNumber(const std::string& str, double& result) {
-    try {
-        size_t pos = 0;
-        result = std::stod(str, &pos);
-        return pos == str.length();
-    } catch (...) {
+bool parse(std::string const & str, DataStruct & o) {
+    if (str[0] != '(')
         return false;
-    }
-}
 
-bool parseNumber(const std::string& str, long long& result) {
-    try {
-        size_t pos = 0;
+    size_t m = 0;
+    for (size_t i = 1; i < str.size(); ++i) {
+        if (str[i] == ':') {
+            std::string key = "";
+            for (i = i + 1; i < str.size() && (str[i] != ' ' && str[i] != ')'); ++i)
+                key += str[i];
 
-        if (str.size() > 2 && str[0] == '0') {
-            if (str[1] == 'x' || str[1] == 'X') {
-                result = std::stoll(str, &pos, 16);
-            } else if (str[1] == 'b' || str[1] == 'B') {
-                result = std::stoll(str.substr(2), &pos, 2);
-            } else {
-                result = std::stoll(str, &pos, 8);
+            if (str[i] == ')' && key.empty()) {
+                if (i == str.size() - 1 && m == 7)
+                    return true;
+
+                return false;
             }
-        } else {
-            result = std::stoll(str, &pos);
-        }
 
-        return pos == str.length();
-    } catch (...) {
-        return false;
+            if (key == "key1") {
+                if (m & 1)
+                    return false;
+                m |= 1;
+
+                std::string value = "";
+                for (i = i + 1; i < str.size() && (str[i] != 'D' && str[i] != 'd'); ++i)
+                    value += str[i];
+
+                if (value.empty())
+                    return false;
+
+                if (value.front() == ' ')
+                    return false;
+
+                if (value.back()  == ' ')
+                    return false;
+
+                o.key1 = std::stod(value);
+
+                continue;
+            }
+
+            if (key == "key2") {
+                if (m & 2)
+                    return false;
+                m |= 2;
+
+                std::string value = "";
+                for (i = i + 1; i < str.size() && (str[i] != 'L' && str[i] != 'l'); ++i)
+                    value += str[i];
+
+                if (value.empty())
+                    return false;
+
+                if (value.front() == ' ')
+                    return false;
+
+                if (value.back()  == ' ')
+                    return false;
+
+                o.key2 = std::stoll(value);
+
+                if (++i == str.size())
+                    return false;
+
+                if (str[i] != 'L' && str[i] != 'l')
+                    return false;
+
+                continue;
+            }
+
+            if (key == "key3") {
+                if (m & 4)
+                    return false;
+                m |= 4;
+
+                if (++i == str.size())
+                    return false;
+
+                if (str[i] != '"')
+                    return false;
+
+                std::string value = "";
+                for (i = i + 1; i < str.size(); ++i) {
+                    if (str[i] == '"' && str[i - 1] != '\\')
+                        break;
+                    value += str[i];
+                }
+
+                o.key3 = value;
+
+                continue;
+            }
+
+            return false;
+        }
     }
+
+    return false;
 }
 
+std::istream& operator>>(std::istream & in, DataStruct & o) {
+    std::string line;
+    std::getline(in, line);
 
-std::ostream& operator<<(std::ostream& out, const DataStruct& o) {
-    out << "(:key1 " << std::fixed << std::setprecision(1) << o.key1 << "D"
-        << ":key2 " << o.key2 << "LL"
-        << ":key3 \"" << o.key3 << "\":)";
+    if (!parse(line, o))
+        throw std::logic_error("Error...");
+
+    return in;
+}
+
+std::ostream& operator<<(std::ostream & out, DataStruct const & o) {
+    out << "(:key1" << " " << std::fixed << std::setprecision(1) << o.key1 << "D"
+        <<  ":key2" << " " << o.key2 << "LL"
+        <<  ":key3" << " " << "\"" << o.key3 << "\":)";
+
     return out;
 }
+
+// (:key1 30.d:key2 -89LL:key3 "Data":)
+// (:key1 10.d:key2 -89LL:key3 "Data":)
+// (:key1 20.d:key2 -89L:key3 "Data":)
 
 int main() {
     std::vector<DataStruct> items;
 
-    try {
-        std::istream_iterator<DataStruct> input_start(std::cin);
-        std::istream_iterator<DataStruct> input_end;
-        std::copy(input_start, input_end, std::back_inserter(items));
-    } catch (const std::exception& e) {
-        std::cerr << "Ошибка чтения: " << e.what() << std::endl;
-        return 1;
+    while (std::cin) {
+        try {
+            std::istream_iterator<DataStruct> input_start(std::cin);
+            std::istream_iterator<DataStruct> input_end;
+            std::copy(input_start, input_end, std::back_inserter(items));
+        }
+        catch (...) {
+
+        }
     }
 
-    std::sort(items.begin(), items.end(), [](const DataStruct& a, const DataStruct& b) {
-        if (a.key1 == b.key1) {
+    std::sort(items.begin(), items.end(), [](auto const & a, auto const & b) {
+        if (a.key1 == b.key2) {
             if (a.key2 == b.key2) {
-                return a.key3.length() < b.key3.length();
+                return a.key3 < b.key3;
             }
             return a.key2 < b.key2;
         }
         return a.key1 < b.key1;
     });
 
-    std::cout << "\nОтсортированные элементы:\n";
-    std::copy(items.begin(), items.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+    std::cout << std::endl;
+
+    std::copy(items.begin(), items.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n"));
+
     std::cout << std::endl;
 
     return 0;
-}
-
-bool parse(const std::string& str, DataStruct& o) {
-    if (str.empty() || str.front() != '(' || str.back() != ')')
-        return false;
-
-    size_t i = 1;
-    int flags = 0;
-
-    while (i < str.size()) {
-        skipSpaces(str, i);
-        if (i >= str.size() || str[i] != ':') break;
-        ++i;
-
-        std::string key;
-        while (i < str.size() && std::isalpha(str[i]))
-            key += str[i++];
-
-        if (key.empty()) return false;
-
-        skipSpaces(str, i);
-        if (i >= str.size()) return false;
-
-        if (key == "key1") {
-            if (flags & 1) return false;
-            flags |= 1;
-
-            std::string val;
-            while (i < str.size() && str[i] != ':' && str[i] != ')') {
-                val += str[i++];
-            }
-
-            if (!val.empty() && (val.back() == 'd' || val.back() == 'D')) {
-                val.pop_back();
-            }
-
-            if (!parseNumber(val, o.key1)) {
-                return false;
-            }
-        }
-        else if (key == "key2") {
-            if (flags & 2) return false;
-            flags |= 2;
-
-            std::string val;
-            while (i < str.size() && str[i] != ':' && str[i] != ')') {
-                val += str[i++];
-            }
-
-            if (val.size() >= 2) {
-                std::string suffix = val.substr(val.size() - 2);
-                for (auto& c : suffix) c = std::tolower(c);
-
-                if (suffix == "ll" || suffix == "ul") {
-                    val.erase(val.size() - 2);
-                }
-            }
-
-            if (!parseNumber(val, o.key2)) {
-                return false;
-            }
-        }
-        else if (key == "key3") {
-            if (flags & 4) return false;
-            flags |= 4;
-
-            if (i >= str.size() || str[i] != '"') return false;
-            ++i;
-
-            std::string val;
-            while (i < str.size()) {
-                if (str[i] == '"' && (i == 0 || str[i - 1] != '\\'))
-                    break;
-                val += str[i++];
-            }
-
-            if (i >= str.size() || str[i] != '"') return false;
-            ++i;
-
-            o.key3 = val;
-        }
-        else {
-            return false;
-        }
-    }
-
-    return (flags == 7);
 }
