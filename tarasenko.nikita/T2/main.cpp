80c0fbf2d964de4df6f32d0cb725e5730f1e6b21
Тарасенко Никита Денисович <niktarasenko26@yandex.ru>
2025-06-07 12:21:16 +0300

    Initial commit for T2

--- tarasenko.nikita/T2/main.cpp
+++ tarasenko.nikita/T2/main.cpp
@@ -1,135 +1,112 @@
 ﻿#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iterator>
 #include <sstream>
 #include <string>
+#include <iomanip>
 #include <cctype>
-#include <climits>
 
 struct DataStruct {
-    unsigned long long key1;  // ULL OCT
-    char key2;                // CHR LIT
+    unsigned long long key1;
+    char key2;
     std::string key3;
 };
 
 std::istream& operator>>(std::istream& in, DataStruct& ds) {
-    DataStruct temp;
-    bool key1_set = false, key2_set = false, key3_set = false;
     std::string line;
+    if (!std::getline(in, line)) {
+        return in;
+    }
 
-    if (!std::getline(in, line, ')')) {
+    size_t start = line.find('(');
+    size_t end = line.rfind(')');
+    if (start == std::string::npos || end == std::string::npos || start >= end) {
         in.setstate(std::ios::failbit);
         return in;
     }
 
-    line += ')';
-    std::istringstream iss(line);
-    char ch;
+    std::string content = line.substr(start + 1, end - start - 1);
+    std::istringstream iss(content);
+    std::string token;
 
-    if (!(iss >> ch) || ch != '(') {
-        in.setstate(std::ios::failbit);
-        return in;
-    }
+    ds.key1 = 0;
+    ds.key2 = '\0';
+    ds.key3.clear();
 
-    std::string key;
-    while (iss >> ch && ch == ':') {
-        if (!(iss >> key)) {
-            in.setstate(std::ios::failbit);
-            return in;
-        }
+    bool key1_found = false, key2_found = false, key3_found = false;
 
-        if (key == "key1") {
+    while (iss >> token) {
+        if (token == ":key1") {
             std::string value;
-            if (!(iss >> value)) {
-                in.setstate(std::ios::failbit);
-                return in;
-            }
+            iss >> value;
             try {
                 size_t pos = 0;
-                temp.key1 = std::stoull(value, &pos, 8);
+                ds.key1 = std::stoull(value, &pos, 8);
                 if (pos != value.size()) {
-                    in.setstate(std::ios::failbit);
-                    return in;
+                    throw std::invalid_argument("Invalid octal number");
                 }
-                key1_set = true;
+                key1_found = true;
             }
             catch (...) {
                 in.setstate(std::ios::failbit);
                 return in;
             }
         }
-        else if (key == "key2") {
+        else if (token == ":key2") {
             std::string value;
-            if (!(iss >> value) || value.size() != 3 || value[0] != '\'' || value[2] != '\'') {
+            iss >> value;
+            if (value.size() >= 3 && value.front() == '\'' && value.back() == '\'') {
+                ds.key2 = value[1];
+                key2_found = true;
+            }
+            else {
                 in.setstate(std::ios::failbit);
                 return in;
             }
-            temp.key2 = value[1];
-            key2_set = true;
         }
-        else if (key == "key3") {
+        else if (token == ":key3") {
             std::string value;
-            iss >> std::ws;
-            if (iss.peek() != '"') {
+            iss >> value;
+            if (value.size() >= 2 && value.front() == '"' && value.back() == '"') {
+                ds.key3 = value.substr(1, value.size() - 2);
+                key3_found = true;
+            }
+            else {
                 in.setstate(std::ios::failbit);
                 return in;
             }
-            iss.get();
-            std::getline(iss, temp.key3, '"');
-            key3_set = true;
-        }
-        else {
-            in.setstate(std::ios::failbit);
-            return in;
-        }
-
-        while (iss.get(ch) && ch != ':') {
-            if (!std::isspace(ch)) {
-                in.setstate(std::ios::failbit);
-                return in;
-            }
-        }
-        if (ch == ':') {
-            iss.putback(ch);
         }
     }
 
-    if (!key1_set || !key2_set || !key3_set) {
+    if (!key1_found || !key2_found || !key3_found) {
         in.setstate(std::ios::failbit);
-        return in;
     }
-
-    ds = temp;
     return in;
 }
 
 std::ostream& operator<<(std::ostream& out, const DataStruct& ds) {
-    out << "(:key1 " << std::oct << ds.key1 << std::dec << "ull"
-        << ":key2 '" << ds.key2 << "'"
-        << ":key3 \"" << ds.key3 << "\":)";
+    out << "(:key1 " << std::oct << ds.key1 << std::dec << ":key2 '" << ds.key2 << "':key3 \"" << ds.key3 << "\":)";
     return out;
 }
 
 bool compareDataStructs(const DataStruct& a, const DataStruct& b) {
-    if (a.key1 != b.key1) return a.key1 < b.key1;
-    if (a.key2 != b.key2) return a.key2 < b.key2;
+    if (a.key1 != b.key1) {
+        return a.key1 < b.key1;
+    }
+    if (a.key2 != b.key2) {
+        return a.key2 < b.key2;
+    }
     return a.key3.length() < b.key3.length();
 }
 
 int main() {
     std::vector<DataStruct> data;
 
-    // Чтение данных с обработкой ошибок
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        std::istringstream iss(line);
-        DataStruct ds;
-        if (iss >> ds) {
-            data.push_back(ds);
-        }
-        // Игнорируем некорректные строки
-    }
+    std::copy(
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data)
+    );
 
     std::sort(data.begin(), data.end(), compareDataStructs);
 
