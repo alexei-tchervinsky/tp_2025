7f08f7a221b2873029b1a0e4ca0c340d6b0bf5d8
я <manykivan23@gmail.com>
2025-06-07 07:12:03 +0300

    try3 T3

--- manyk.ivan/T3/main.cpp
+++ manyk.ivan/T3/main.cpp
@@ -42,6 +42,8 @@ int main(int argc, char **argv)
         }
         else
         {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "<INVALID COMMAND>\n";
         }
     }
