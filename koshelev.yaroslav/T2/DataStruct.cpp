312c1c41329e35266ef1190eb011be3b4161b9e8
senku812 <xaomi29122017@gmail.com>
2025-06-07 18:15:12 +0300

    Update DataStruct.cpp

--- koshelev.yaroslav/T2/DataStruct.cpp
+++ koshelev.yaroslav/T2/DataStruct.cpp
@@ -1,14 +1,20 @@
 #include "DataStruct.h"
 #include <sstream>
+#include <cctype>
 
 bool parseScientificDouble(const std::string& str, double& value) {
     std::istringstream iss(str);
     iss >> value;
-    return !iss.fail();
+    return !iss.fail() && iss.eof();
 }
 
 bool parseHexULL(const std::string& str, unsigned long long& value) {
-    std::istringstream iss(str);
+    std::string s = str;
+    if (s.size() > 1 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) {
+        s = s.substr(2);
+    }
+    
+    std::istringstream iss(s);
     iss >> std::hex >> value;
-    return !iss.fail();
+    return !iss.fail() && iss.eof();
 }
