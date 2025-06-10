18c235e47084e3158fb7cec4af4f901abc37fee1
Maria-Mariaa <sm4riia@yandex.ru>
2025-06-10 15:49:06 +0300

    att8

--- shubina.maria/T2/main.cpp
+++ shubina.maria/T2/main.cpp
@@ -2,7 +2,82 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include "DataStruct.h"
+#include <sstream>
+
+struct DataStruct {
+    std::string key1;
+    std::string key2;
+    std::string key3;
+};
+
+std::istream& operator>>(std::istream& in, DataStruct& data) {
+    char ch;
+
+    if (!(in >> ch) || ch != '(') {
+        in.setstate(std::ios_base::failbit);
+        return in;
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        if (!(in >> ch) || ch != ':') {
+            in.setstate(std::ios_base::failbit);
+            return in;
+        }
+
+        std::string key;
+        in >> key;
+
+        if (!(in >> ch) || ch != ':') {
+            in.setstate(std::ios_base::failbit);
+            return in;
+        }
+
+        std::ostringstream valueStream;
+        in >> std::noskipws;
+
+        int depth = 0;
+        while (in >> ch) {
+            if (ch == '(') depth++;
+            else if (ch == ')') {
+                if (--depth == 0) break;
+            } else if (ch == ':' && depth == 0) {
+                in.putback(ch);
+                break;
+            }
+            valueStream << ch;
+        }
+
+        if (key == "key1") {
+            data.key1 = valueStream.str();
+        } else if (key == "key2") {
+            data.key2 = valueStream.str();
+        } else if (key == "key3") {
+            data.key3 = valueStream.str();
+        } else {
+            in.setstate(std::ios_base::failbit);
+            return in;
+        }
+    }
+
+    if (!(in >> ch) || ch != ')') {
+        in.setstate(std::ios_base::failbit);
+    }
+
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
+    out << "(:key1 " << data.key1 << ":"
+        << ":key2 " << data.key2 << ":"
+        << ":key3 " << data.key3 << ":)";
+    return out;
+}
+
+bool operator<(const DataStruct& a, const DataStruct& b) {
+    if (a.key1 != b.key1) return a.key1 < b.key1;
+    if (a.key2 != b.key2) return a.key2 < b.key2;
+    return a.key3.length() < b.key3.length();
+}
 
 int main() {
     std::vector<DataStruct> dataVector;
@@ -18,13 +93,7 @@ int main() {
         return 0;
     }
 
-    auto compare = [](const DataStruct& a, const DataStruct& b) {
-        if (a.key1 != b.key1) return a.key1 < b.key1;
-        if (a.key2 != b.key2) return a.key2 < b.key2;
-        return a.key3.length() < b.key3.length();
-    };
-
-    std::sort(dataVector.begin(), dataVector.end(), compare);
+    std::sort(dataVector.begin(), dataVector.end());
 
     std::copy(
         dataVector.begin(),
