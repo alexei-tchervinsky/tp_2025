9fbec7c1c831c61b2946b94a5a312579a08f0770
senku812 <xaomi29122017@gmail.com>
2025-06-12 16:14:24 +0300

    Update main.cpp

--- koshelev.yaroslav/T2/main.cpp
+++ koshelev.yaroslav/T2/main.cpp
@@ -1,28 +1,23 @@
+#include "DataStruct.h"
+#include "IO_Objects.h"
+#include "iofmtguard.h"
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <iterator>
-#include "iofmtguard.h"
-#include "DataStruct.h"
 
 int main() {
-    using namespace solution;
+    solution::iofmtguard guard(std::cout);
+    std::vector<solution::DataStruct> data;
 
-    std::vector<DataStruct> data;
-    std::istream_iterator<DataStruct> inputIt(std::cin);
-    std::istream_iterator<DataStruct> eos;
-
-    std::copy(inputIt, eos, std::back_inserter(data));
+    solution::readData(std::cin, data);
 
     if (data.empty()) {
-        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
+        std::cout << "No valid data found.\n";
         return 0;
     }
 
-    std::sort(data.begin(), data.end());
-
-    std::ostream_iterator<DataStruct> outputIt(std::cout, "\n");
-    std::copy(data.begin(), data.end(), outputIt);
+    solution::sortData(data);
+    solution::writeData(std::cout, data);
 
     return 0;
 }
+
