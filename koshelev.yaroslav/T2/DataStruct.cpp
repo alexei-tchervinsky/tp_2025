ba0f6d7d3a1fc12f2b28eebb5f5886a5f813d6f3
senku812 <xaomi29122017@gmail.com>
2025-06-12 16:12:10 +0300

    Update DataStruct.cpp

--- koshelev.yaroslav/T2/DataStruct.cpp
+++ koshelev.yaroslav/T2/DataStruct.cpp
@@ -1,64 +1,118 @@
 #include "DataStruct.h"
-#include "iofmtguard.h"
 #include <iomanip>
 #include <sstream>
+#include <algorithm>
 
 namespace solution {
 
-    bool operator<(const DataStruct& lhs, const DataStruct& rhs) {
-        if (lhs.key1 != rhs.key1) return lhs.key1 < rhs.key1;
-        if (lhs.key2 != rhs.key2) return lhs.key2 < rhs.key2;
-        return lhs.key3 < rhs.key3;
+  std::istream& operator>>(std::istream& in, DataStruct& data) {
+    std::string line;
+    std::getline(in, line);
+
+    if (line.empty()) {
+      in.setstate(std::ios::failbit);
+      return in;
     }
 
-    std::istream& operator>>(std::istream& in, DataStruct& data) {
-        std::string input;
-        if (!std::getline(in, input, '\n')) return in;
-
-        std::istringstream ss(input);
-        std::string token;
-
-        size_t pos1 = input.find(":key1 ");
-        size_t pos2 = input.find(":key2 ");
-        size_t pos3 = input.find(":key3 ");
-
-        if (pos1 == std::string::npos || pos2 == std::string::npos || pos3 == std::string::npos) return in;
-
-        try {
-            std::string substr1 = input.substr(pos1 + 7, pos2 - (pos1 + 7));
-            std::string substr2 = input.substr(pos2 + 7, pos3 - (pos2 + 7));
-            std::string substr3 = input.substr(pos3 + 7);
-
-            substr3.erase(std::remove(substr3.begin(), substr3.end(), ':'), substr3.end());
-            substr3.erase(std::remove(substr3.begin(), substr3.end(), ')'), substr3.end());
-            if (substr3.front() == '"' && substr3.back() == '"') {
-                substr3 = substr3.substr(1, substr3.length() - 2);
-            }
-
-            data.key1 = std::stod(substr1);
-
-            if (substr2.rfind("0x", 0) == 0 || substr2.rfind("0X", 0) == 0)
-                data.key2 = std::stoull(substr2, nullptr, 16);
-            else if (substr2.find("ull") != std::string::npos)
-                data.key2 = std::stoull(substr2);
-            else
-                return in;
-
-            data.key3 = substr3;
-        } catch (...) {
-            return in;
-        }
-
-        return in;
+    std::istringstream iss(line);
+    std::string temp;
+    if (!(iss >> temp) || temp != "(:key1") {
+      in.setstate(std::ios::failbit);
+      return in;
     }
 
-    std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
-        iofmtguard guard(out);
-        out << std::scientific << std::uppercase << std::setprecision(1);
-        out << "(:key1 " << data.key1;
-        out << ":key2 0X" << std::hex << std::uppercase << data.key2;
-        out << ":key3 \"" << data.key3 << "\":)";
-        return out;
+    if (!(iss >> temp) || temp.substr(0, 2) != "#c") {
+      in.setstate(std::ios::failbit);
+      return in;
     }
+
+    temp.erase(0, 3);
+    std::string real_str, imag_str;
+    std::istringstream(temp) >> real_str;
+
+    if (!(iss >> imag_str)) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    imag_str.pop_back();
+    try {
+      data.key1 = std::complex<double>(std::stod(real_str), std::stod(imag_str));
+    } catch (...) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    if (!(iss >> temp) || temp != ":key2") {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    if (!(iss >> temp) || temp != "(:N") {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    long long numerator;
+    unsigned long long denominator;
+    if (!(iss >> numerator)) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    if (!(iss >> temp) || temp != ":D") {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    if (!(iss >> std::hex >> denominator)) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    if (!(iss >> temp) || temp != ":)") {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    data.key2 = {numerator, denominator};
+
+    if (!(iss >> temp) || temp != ":key3") {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    std::string str;
+    if (!(iss >> std::quoted(str))) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    if (!(iss >> temp) || temp != ":)") {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    data.key3 = str;
+    return in;
+  }
+
+  std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
+    out << "(:key1 #c(" << std::scientific << data.key1.real() << ' '
+        << data.key1.imag() << ")"
+        << ":key2 (:N " << std::dec << data.key2.first
+        << ":D " << std::uppercase << std::hex << data.key2.second << ":)"
+        << ":key3 " << std::quoted(data.key3) << ":)";
+    return out;
+  }
+
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs) {
+    if (lhs.key1 != rhs.key1)
+      return lhs.key1.real() < rhs.key1.real() ||
+            (lhs.key1.real() == rhs.key1.real() && lhs.key1.imag() < rhs.key1.imag());
+    if (lhs.key2 != rhs.key2)
+      return lhs.key2 < rhs.key2;
+    return lhs.key3.length() < rhs.key3.length();
+  }
 
 }
