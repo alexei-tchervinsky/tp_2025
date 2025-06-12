a6db553de4ff75c1b89b43e06c318ae29bac2b13
senku812 <xaomi29122017@gmail.com>
2025-06-12 15:30:22 +0300

    Update iofmtguard.h

--- koshelev.yaroslav/T2/iofmtguard.h
+++ koshelev.yaroslav/T2/iofmtguard.h
@@ -1,19 +1,21 @@
 #ifndef IOFMTGUARD_H
 #define IOFMTGUARD_H
 
-#include <ios>
-#include <iomanip>
+#include <iostream>
 
-class iofmtguard {
-public:
-    explicit iofmtguard(std::basic_ios<char>& s);
-    ~iofmtguard();
+namespace solution {
 
-private:
-    std::basic_ios<char>& s_;
-    char fill_;
-    std::streamsize precision_;
-    std::basic_ios<char>::fmtflags fmt_;
-};
+    class iofmtguard {
+    public:
+        explicit iofmtguard(std::basic_ios<char>& stream);
+        ~iofmtguard();
+    private:
+        std::basic_ios<char>& stream_;
+        char fill_;
+        std::streamsize precision_;
+        std::basic_ios<char>::fmtflags flags_;
+    };
+
+}
 
 #endif
