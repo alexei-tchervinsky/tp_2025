230773449f2e61bb34c52f10d83bb4f85032190b
senku812 <xaomi29122017@gmail.com>
2025-06-12 15:56:10 +0300

    Update DataStruct.cpp

--- koshelev.yaroslav/T2/DataStruct.cpp
+++ koshelev.yaroslav/T2/DataStruct.cpp
@@ -1,65 +1,63 @@
-#include "DataStruct.h"
-#include "IO_Objects.h"
-#include "iofmtguard.h"
+#include "DataStruct.hpp"
+#include "iofmtguard.hpp"
 #include <iomanip>
 #include <sstream>
-#include <cctype>
 
 namespace solution {
 
-    bool DataStruct::operator<(const DataStruct& other) const {
-        if (key1 != other.key1) {
-            return key1 < other.key1;
-        }
-        if (key2 != other.key2) {
-            return key2 < other.key2;
-        }
-        return key3.size() < other.key3.size();
+    bool operator<(const DataStruct& lhs, const DataStruct& rhs) {
+        if (lhs.key1 != rhs.key1) return lhs.key1 < rhs.key1;
+        if (lhs.key2 != rhs.key2) return lhs.key2 < rhs.key2;
+        return lhs.key3 < rhs.key3;
     }
 
     std::istream& operator>>(std::istream& in, DataStruct& data) {
-        std::istream::sentry guard(in);
-        if (!guard) return in;
+        std::string input;
+        if (!std::getline(in, input, '\n')) return in;
 
-        DataStruct temp;
-        bool hasKey1 = false, hasKey2 = false, hasKey3 = false;
+        std::istringstream ss(input);
+        std::string token;
 
-        in >> DelimiterIO{'('} >> DelimiterIO{':'};
-        for (int i = 0; i < 3 && in; ++i) {
-            std::string label;
-            in >> LabelIO{label};
+        size_t pos1 = input.find(":key1 ");
+        size_t pos2 = input.find(":key2 ");
+        size_t pos3 = input.find(":key3 ");
 
-            if (label == "key1") {
-                in >> DoubleSciIO{temp.key1} >> DelimiterIO{':'};
-                hasKey1 = in.good();
-            } else if (label == "key2") {
-                in >> ULLHexIO{temp.key2} >> DelimiterIO{':'};
-                hasKey2 = in.good();
-            } else if (label == "key3") {
-                in >> StringIO{temp.key3} >> DelimiterIO{':'};
-                hasKey3 = in.good();
-            } else {
-                std::string skip;
-                std::getline(in, skip, ':');
+        if (pos1 == std::string::npos || pos2 == std::string::npos || pos3 == std::string::npos) return in;
+
+        try {
+            std::string substr1 = input.substr(pos1 + 7, pos2 - (pos1 + 7));
+            std::string substr2 = input.substr(pos2 + 7, pos3 - (pos2 + 7));
+            std::string substr3 = input.substr(pos3 + 7);
+
+            substr3.erase(std::remove(substr3.begin(), substr3.end(), ':'), substr3.end());
+            substr3.erase(std::remove(substr3.begin(), substr3.end(), ')'), substr3.end());
+            if (substr3.front() == '"' && substr3.back() == '"') {
+                substr3 = substr3.substr(1, substr3.length() - 2);
             }
-        }
 
-        in >> DelimiterIO{')'};
-        if (hasKey1 && hasKey2 && hasKey3) {
-            data = temp;
-        } else {
-            in.setstate(std::ios::failbit);
+            data.key1 = std::stod(substr1);
+
+            if (substr2.rfind("0x", 0) == 0 || substr2.rfind("0X", 0) == 0)
+                data.key2 = std::stoull(substr2, nullptr, 16);
+            else if (substr2.find("ull") != std::string::npos)
+                data.key2 = std::stoull(substr2);
+            else
+                return in;
+
+            data.key3 = substr3;
+        } catch (...) {
+            return in;
         }
 
         return in;
     }
 
     std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
-        iofmtguard fmt(out);
-        out << "(:";
-        out << "key1 " << std::scientific << std::uppercase << std::setprecision(6) << data.key1 << ":";
-        out << "key2 0X" << std::uppercase << std::hex << data.key2 << ":";
-        out << "key3 " << std::quoted(data.key3) << ":)";
+        iofmtguard guard(out);
+        out << std::scientific << std::uppercase << std::setprecision(1);
+        out << "(:key1 " << data.key1;
+        out << ":key2 0X" << std::hex << std::uppercase << data.key2;
+        out << ":key3 \"" << data.key3 << "\":)";
         return out;
     }
 
