69234813f6f493ea6db7fe366143db30648416b3
senku812 <xaomi29122017@gmail.com>
2025-06-12 17:04:42 +0300

    Second commit

---
+++ koshelev.yaroslav/T2/DataStruct.hpp
@@ -0,0 +1,23 @@
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
+
+#include <complex>
+#include <string>
+#include <iostream>
+
+namespace solution
+{
+    struct DataStruct
+    {
+        std::complex<double> key1;
+        char key2;
+        std::string key3;
+
+        bool operator<(const DataStruct& other) const;
+    };
+
+    std::istream& operator>>(std::istream& in, DataStruct& value);
+    std::ostream& operator<<(std::ostream& out, const DataStruct& value);
+}
+
+#endif
---
+++ koshelev.yaroslav/T2/IO_Objects.hpp
@@ -0,0 +1,42 @@
+#ifndef IO_OBJECTS_HPP
+#define IO_OBJECTS_HPP
+
+#include <string>
+#include <complex>
+#include <iostream>
+
+namespace solution
+{
+    struct DelimiterIO
+    {
+        char expected;
+    };
+
+    struct LabelIO
+    {
+        std::string& ref;
+    };
+
+    struct ComplexIO
+    {
+        std::complex<double>& ref;
+    };
+
+    struct CharIO
+    {
+        char& ref;
+    };
+
+    struct StringIO
+    {
+        std::string& ref;
+    };
+
+    std::istream& operator>>(std::istream& in, DelimiterIO&&);
+    std::istream& operator>>(std::istream& in, LabelIO&&);
+    std::istream& operator>>(std::istream& in, ComplexIO&&);
+    std::istream& operator>>(std::istream& in, CharIO&&);
+    std::istream& operator>>(std::istream& in, StringIO&&);
+}
+
+#endif
