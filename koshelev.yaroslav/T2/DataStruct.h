a19cd45a4d974f50f8a20fe500c71f12598b3e80
senku812 <xaomi29122017@gmail.com>
2025-06-12 16:37:43 +0300

    Update DataStruct.h

--- koshelev.yaroslav/T2/DataStruct.h
+++ koshelev.yaroslav/T2/DataStruct.h
@@ -1,21 +1,23 @@
 #ifndef DATASTRUCT_H
 #define DATASTRUCT_H
 
-#include <string>
 #include <complex>
-#include <utility>
+#include <string>
 #include <iostream>
 
-namespace solution {
-    struct DataStruct {
-        std::complex<double> key1;
-        std::pair<long long, unsigned long long> key2;
-        std::string key3;
-    };
+namespace solution
+{
+  struct DataStruct
+  {
+    std::complex<double> key1;
+    char key2;
+    std::string key3;
 
-    std::istream& operator>>(std::istream& in, DataStruct& data);
-    std::ostream& operator<<(std::ostream& out, const DataStruct& data);
-    bool operator<(const DataStruct& lhs, const DataStruct& rhs);
+    bool operator<(const DataStruct& other) const;
+  };
+
+  std::istream& operator>>(std::istream& in, DataStruct& value);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& value);
 }
 
 #endif
