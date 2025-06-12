8326781bb2ad61a03d8122befadf58af3bf5a5c2
senku812 <xaomi29122017@gmail.com>
2025-06-12 16:38:36 +0300

    Update IO_Objects.cpp

--- koshelev.yaroslav/T2/IO_Objects.cpp
+++ koshelev.yaroslav/T2/IO_Objects.cpp
@@ -1,23 +1,87 @@
 #include "IO_Objects.h"
-#include <algorithm>
 
-namespace solution {
+#include <iomanip>
+#include <sstream>
 
-  void readData(std::istream& in, std::vector<DataStruct>& data) {
-    DataStruct item;
-    while (in >> item) {
-      data.push_back(item);
+namespace solution
+{
+  std::istream& operator>>(std::istream& in, DelimiterIO&& d)
+  {
+    char c;
+    in >> std::ws >> c;
+    if (!in || c != d.expected)
+    {
+      in.setstate(std::ios::failbit);
     }
+    return in;
   }
 
-  void sortData(std::vector<DataStruct>& data) {
-    std::sort(data.begin(), data.end());
-  }
-
-  void writeData(std::ostream& out, const std::vector<DataStruct>& data) {
-    for (const auto& item : data) {
-      out << item << '\n';
+  std::istream& operator>>(std::istream& in, LabelIO&& l)
+  {
+    l.ref.clear();
+    char c;
+    while (in.get(c))
+    {
+      if (c == ' ')
+      {
+        break;
+      }
+      l.ref += c;
     }
+    if (l.ref.empty())
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
   }
 
+  std::istream& operator>>(std::istream& in, ComplexIO&& c)
+  {
+    char hash, ch, open, close;
+    double real, imag;
+    in >> hash >> ch >> open;
+    if (hash != '#' || ch != 'c' || open != '(')
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    in >> real >> imag >> close;
+    if (!in || close != ')')
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    c.ref = std::complex<double>{real, imag};
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, CharIO&& c)
+  {
+    char quote1, ch, quote2;
+    in >> quote1 >> ch >> quote2;
+    if (quote1 != '\'' || quote2 != '\'')
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    c.ref = ch;
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, StringIO&& s)
+  {
+    char quote;
+    in >> std::ws >> quote;
+    if (quote != '"')
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    std::getline(in, s.ref, '"');
+    if (!in)
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
 }
