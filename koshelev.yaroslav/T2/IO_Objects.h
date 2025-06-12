5944598a1796afb145810afc1fd56e33906f95c6
senku812 <xaomi29122017@gmail.com>
2025-06-12 16:13:31 +0300

    Update IO_Objects.h

--- koshelev.yaroslav/T2/IO_Objects.h
+++ koshelev.yaroslav/T2/IO_Objects.h
@@ -1,37 +1,13 @@
 #ifndef IO_OBJECTS_H
 #define IO_OBJECTS_H
 
-#include <string>
-#include <iostream>
+#include <vector>
+#include "DataStruct.h"
 
 namespace solution {
-
-    struct DelimiterIO {
-        char expected;
-    };
-
-    struct LabelIO {
-        std::string& ref;
-    };
-
-    struct DoubleSciIO {
-        double& ref;
-    };
-
-    struct ULLHexIO {
-        unsigned long long& ref;
-    };
-
-    struct StringIO {
-        std::string& ref;
-    };
-
-    std::istream& operator>>(std::istream& in, DelimiterIO&&);
-    std::istream& operator>>(std::istream& in, LabelIO&&);
-    std::istream& operator>>(std::istream& in, DoubleSciIO&&);
-    std::istream& operator>>(std::istream& in, ULLHexIO&&);
-    std::istream& operator>>(std::istream& in, StringIO&&);
-
+    void readData(std::istream& in, std::vector<DataStruct>& data);
+    void sortData(std::vector<DataStruct>& data);
+    void writeData(std::ostream& out, const std::vector<DataStruct>& data);
 }
 
 #endif
