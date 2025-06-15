a73065c43a746bcc0beefba0a8d9319e57ba3757
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2025-06-15 16:29:29 +0300

    Обновление T3

--- prokopenko.nikita/T3/main.cpp
+++ prokopenko.nikita/T3/main.cpp
@@ -1,70 +1,106 @@
 ﻿#include <iostream>
+#include <map>
+#include <vector>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <algorithm> // для std::all_of
-#include "polygon.hpp"
+#include <limits>
+#include <functional>
+#include <algorithm>
+#include <tuple>
+#include <cctype>
 #include "commands.hpp"
+#include "polygon.hpp"
 
 using namespace prokopenko;
 
 int main(int argc, char* argv[]) {
-  (void)argv;
-  if (argc != 1) {
-    std::cerr << "Unexpected arguments\n";
-    return 1;
-  }
+  std::vector<Polygon> polygons;
 
-  std::vector<Polygon> polys;
-  Polygon p;
-  while (std::cin >> p) {
-    polys.push_back(p);
-  }
+  if (argc == 2) {
+    std::ifstream input(argv[1]);
+    if (!input) {
+      std::cerr << "Error: file cannot be opened\n";
+      return 1;
+    }
 
-  std::cin.clear(); // сброс флага EOF для последующего чтения команд
-  std::string cmd;
-
-  std::cout << "With data:\n";
-  for (const auto& poly : polys) {
-    std::cout << '\t' << poly << '\n';
-  }
-
-  while (std::cin >> cmd) {
-    if (cmd == "COUNT") {
-      std::string arg;
-      if (!(std::cin >> arg)) {
-        std::cout << "<INVALID COMMAND>\n";
-        continue;
-      }
-      if (arg == "EVEN") CountEven(polys, std::cout);
-      else if (arg == "ODD") CountOdd(polys, std::cout);
-      else if (std::all_of(arg.begin(), arg.end(), ::isdigit)) {
-        size_t n = std::stoul(arg);
-        CountN(polys, std::cout, n);
+    while (!input.eof()) {
+      Polygon poly;
+      std::streampos pos = input.tellg();
+      if (input >> poly) {
+        double a = poly.getArea();
+        if (a > 1e-6) {
+          polygons.push_back(poly);
+        }
       }
       else {
-        std::cout << "<INVALID COMMAND>\n";
+        input.clear();
+        input.seekg(pos);
+        input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       }
     }
-    else if (cmd == "AREA") Area(polys, std::cout);
-    else if (cmd == "MAX") Max(polys, std::cout);
-    else if (cmd == "MIN") Min(polys, std::cout);
-    else if (cmd == "MEAN") Mean(polys, std::cout);
-    else if (cmd == "SAME") Same(polys, std::cout);
-    else if (cmd == "RIGHT") Right(polys, std::cout);
-    else if (cmd == "PERMS") Perms(polys, std::cout);
-    else if (cmd == "LESS") Less(polys, std::cout);
-    else if (cmd == "MORE") More(polys, std::cout);
-    else if (cmd == "EQUAL") Equal(polys, std::cout);
-    else if (cmd == "MAXSEQ") MaxSeq(polys, std::cout);
-    else if (cmd == "RMECHO") RmEcho(polys, std::cout);
-    else if (cmd == "ECHO") EchoCmd(polys, std::cout);
-    else if (cmd == "LESSAREA") LessArea(polys, std::cout);
-    else if (cmd == "INFRAME") InFrame(polys, std::cout);
-    else if (cmd == "INTERSECTIONS") Intersections(polys, std::cout);
-    else if (cmd == "RECTS") Rects(polys, std::cout);
-    else if (cmd == "RIGHTSHAPES") RightShapes(polys, std::cout);
-    else std::cout << "<INVALID COMMAND>\n";
+
+    std::cout << "With data:\n";
+    for (const auto& p : polygons) {
+      std::cout << "\t" << p << "\n";
+    }
+  }
+
+  std::map<std::string, std::function<void(const std::vector<Polygon>&, std::ostream&)>> commands;
+  commands["AREA"] = Area;
+  commands["MAX"] = Max;
+  commands["MIN"] = Min;
+  commands["MEAN"] = Mean;
+  commands["SAME"] = Same;
+  commands["RIGHT"] = Right;
+  commands["PERMS"] = Perms;
+  commands["LESS"] = Less;
+  commands["MORE"] = More;
+  commands["EQUAL"] = Equal;
+  commands["MAXSEQ"] = MaxSeq;
+  commands["RMECHO"] = RmEcho;
+  commands["ECHO"] = EchoCmd;
+  commands["LESSAREA"] = LessArea;
+  commands["INFRAME"] = InFrame;
+  commands["INTERSECTIONS"] = Intersections;
+  commands["RECTS"] = Rects;
+  commands["RIGHTSHAPES"] = RightShapes;
+
+  commands["COUNT"] = [](const std::vector<Polygon>& polys, std::ostream& out) {
+    std::string param;
+    if (!(std::cin >> param)) {
+      out << "<INVALID COMMAND>\n";
+      return;
+    }
+    if (param == "ODD") {
+      CountOdd(polys, out);
+    }
+    else if (param == "EVEN") {
+      CountEven(polys, out);
+    }
+    else if (!param.empty() && std::all_of(param.begin(), param.end(), ::isdigit)) {
+      try {
+        size_t n = std::stoul(param);
+        CountN(polys, out, n);
+      }
+      catch (...) {
+        out << "<INVALID COMMAND>\n";
+      }
+    }
+    else {
+      out << "<INVALID COMMAND>\n";
+    }
+    };
+
+  std::string cmd;
+  while (std::cin >> cmd) {
+    auto it = commands.find(cmd);
+    if (it != commands.end()) {
+      it->second(polygons, std::cout);
+    }
+    else {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
   }
 
   return 0;
